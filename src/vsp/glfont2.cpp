cebcce1e76d5feb5cfcbd62960ea8a60a48f8c8e
Rob McDonald <ramcdona@calpoly.edu>
2012-01-10 11:01:43 -0800

    Include GLFont2 for displaying 2D text in OpenGL.
    GLFont2 also requires the original distribution zip file be distributed with the
    program.

---
+++ src/vsp/glfont2.cpp
@@ -0,0 +1,204 @@
+//*******************************************************************
+//glfont2.cpp -- glFont Version 2.0 implementation
+//Copyright (c) 1998-2002 Brad Fish
+//See glfont.html for terms of use
+//May 14, 2002
+//*******************************************************************
+
+//STL headers
+#include <string>
+#include <utility>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+//glFont header
+#include "glfont2.h"
+using namespace glfont;
+
+//*******************************************************************
+//GLFont Class Implementation
+//*******************************************************************
+GLFont::GLFont ()
+{
+	//Initialize header to safe state
+	header.tex = -1;
+	header.tex_width = 0;
+	header.tex_height = 0;
+	header.start_char = 0;
+	header.end_char = 0;
+	header.chars = NULL;
+}
+//*******************************************************************
+GLFont::~GLFont ()
+{
+	//Destroy the font
+	Destroy();
+}
+//*******************************************************************
+bool GLFont::Create (const char *file_name, int tex)
+{
+	ifstream input;
+	int num_chars, num_tex_bytes;
+	char *tex_bytes;
+
+	//Destroy the old font if there was one, just to be safe
+	Destroy();
+
+	//Open input file
+	input.open(file_name, ios::in | ios::binary);
+	if (!input)
+		return false;
+
+	//Read the header from file
+	input.read((char *)&header, sizeof(header));
+	header.tex = tex;
+
+	//Allocate space for character array
+	num_chars = header.end_char - header.start_char + 1;
+	if ((header.chars = new GLFontChar[num_chars]) == NULL)
+		return false;
+
+	//Read character array
+	input.read((char *)header.chars, sizeof(GLFontChar) *
+		num_chars);
+
+	//Read texture pixel data
+	num_tex_bytes = header.tex_width * header.tex_height * 2;
+	tex_bytes = new char[num_tex_bytes];
+	input.read(tex_bytes, num_tex_bytes);
+
+	//Create OpenGL texture
+	glBindTexture(GL_TEXTURE_2D, tex);  
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+	glTexImage2D(GL_TEXTURE_2D, 0, 2, header.tex_width,
+		header.tex_height, 0, GL_LUMINANCE_ALPHA, GL_UNSIGNED_BYTE,
+		(void *)tex_bytes);
+
+	//Free texture pixels memory
+	delete[] tex_bytes;
+
+	//Close input file
+	input.close();
+
+	//Return successfully
+	return true;
+}
+//*******************************************************************
+bool GLFont::Create (const std::string &file_name, int tex)
+{
+	return Create(file_name.c_str(), tex);
+}
+//*******************************************************************
+void GLFont::Destroy (void)
+{
+	//Delete the character array if necessary
+	if (header.chars)
+	{
+		delete[] header.chars;
+		header.chars = NULL;
+	}
+}
+//*******************************************************************
+void GLFont::GetTexSize (std::pair<int, int> *size)
+{
+	//Retrieve texture size
+	size->first = header.tex_width;
+	size->second = header.tex_height;
+}
+//*******************************************************************
+int GLFont::GetTexWidth (void)
+{
+	//Return texture width
+	return header.tex_width;
+}
+//*******************************************************************
+int GLFont::GetTexHeight (void)
+{
+	//Return texture height
+	return header.tex_height;
+}
+//*******************************************************************
+void GLFont::GetCharInterval (std::pair<int, int> *interval)
+{
+	//Retrieve character interval
+	interval->first = header.start_char;
+	interval->second = header.end_char;
+}
+//*******************************************************************
+int GLFont::GetStartChar (void)
+{
+	//Return start character
+	return header.start_char;
+}
+//*******************************************************************
+int GLFont::GetEndChar (void)
+{
+	//Return end character
+	return header.end_char;
+}
+//*******************************************************************
+void GLFont::GetCharSize (int c, std::pair<int, int> *size)
+{
+	//Make sure character is in range
+	if (c < header.start_char || c > header.end_char)
+	{
+		//Not a valid character, so it obviously has no size
+		size->first = 0;
+		size->second = 0;
+	}
+	else
+	{
+		GLFontChar *glfont_char;
+
+		//Retrieve character size
+		glfont_char = &header.chars[c - header.start_char];
+		size->first = (int)(glfont_char->dx * header.tex_width);
+		size->second = (int)(glfont_char->dy *
+			header.tex_height);
+	}
+}
+//*******************************************************************
+int GLFont::GetCharWidth (int c)
+{
+	//Make sure in range
+	if (c < header.start_char || c > header.end_char)
+		return 0;
+	else
+	{
+		GLFontChar *glfont_char;
+		
+		//Retrieve character width
+		glfont_char = &header.chars[c - header.start_char];
+		return (int)(glfont_char->dx * header.tex_width);
+	}
+}
+//*******************************************************************
+int GLFont::GetCharHeight (int c)
+{
+	//Make sure in range
+	if (c < header.start_char || c > header.end_char)
+		return 0;
+	else
+	{
+		GLFontChar *glfont_char;
+
+		//Retrieve character height
+		glfont_char = &header.chars[c - header.start_char];
+		return (int)(glfont_char->dy * header.tex_height);
+	}
+}
+//*******************************************************************
+void GLFont::Begin (void)
+{
+	//Bind to font texture
+	glBindTexture(GL_TEXTURE_2D, header.tex);
+}
+//*******************************************************************
+
+//End of file
+
---
+++ src/vsp/glfont2.h
@@ -0,0 +1,527 @@
+//*******************************************************************
+//glfont2.h -- Header for glfont2.cpp
+//Copyright (c) 1998-2002 Brad Fish
+//See glfont.html for terms of use
+//May 14, 2002
+//*******************************************************************
+
+#ifndef GLFONT2_H
+#define GLFONT2_H
+
+#pragma warning(disable:4786)
+
+#ifdef WIN32
+#include <windows.h>		
+#endif
+
+#ifdef __APPLE__
+#  include <OpenGL/gl.h>
+#else
+#  include <GL/gl.h>
+#endif
+
+#include <string>
+
+//*******************************************************************
+//GLFont Interface
+//*******************************************************************
+
+//glFont namespace
+namespace glfont
+{
+	class GLFont;	
+}
+
+//glFont class
+class glfont::GLFont
+{
+private:
+
+	//glFont character structure
+	typedef struct
+	{
+		float dx, dy;
+		float tx1, ty1;
+		float tx2, ty2;
+	} GLFontChar;
+
+	//glFont header structure
+	struct
+	{
+		int tex;
+		int tex_width, tex_height;
+		int start_char, end_char;
+		GLFontChar *chars;
+	} header;
+
+public:
+
+	//Constructor
+	GLFont ();
+
+	//Destructor
+	~GLFont ();
+
+public:
+
+	//Creates the glFont
+	bool Create (const char *file_name, int tex);
+	bool Create (const std::string &file_name, int tex);
+
+	//Destroys the glFont
+	void Destroy (void);
+
+	//Texture size retrieval methods
+	void GetTexSize (std::pair<int, int> *size);
+	int GetTexWidth (void);
+	int GetTexHeight (void);
+
+	//Character interval retrieval methods
+	void GetCharInterval (std::pair<int, int> *interval);
+	int GetStartChar (void);
+	int GetEndChar (void);
+
+	//Character size retrieval methods
+	void GetCharSize (int c, std::pair<int, int> *size);
+	int GetCharWidth (int c);
+	int GetCharHeight (int c);
+
+	//Calculates the size in pixels of a character array
+	template<class T> void GetStringSize (const T *text,
+		std::pair<int, int> *size)
+	{
+		const T *i;
+		GLFontChar *glfont_char;
+		float width;
+		
+		//Height is the same for now...might change in future
+		size->second = (int)(header.chars[header.start_char].dy *
+			header.tex_height);
+
+		//Calculate width of string
+		width = 0.0F;
+		for (i = text; *i != (T)'\0'; i++)
+		{
+			//Make sure character is in range
+			if (*i < header.start_char || *i > header.end_char)
+				continue;
+
+			//Get pointer to glFont character
+			glfont_char = &header.chars[*i - header.start_char];
+
+			//Get width and height
+			width += glfont_char->dx * header.tex_width;		
+		}
+
+		//Save width
+		size->first = (int)width;
+	}
+	
+	//Template function to calculate size of a std::basic_string
+	template<class T> void GetStringSize (
+		const std::basic_string<T> &text, std::pair<int, int> *size)
+	{
+		unsigned int i;
+		T *c;
+		GLFontChar *glfont_char;
+		float width;
+		
+		//Height is the same for now...might change in future
+		size->second = (int)(header.chars[header.start_char].dy *
+			header.tex_height);
+
+		//Calculate width of string
+		width = 0.0F;
+		for (i = 0; i < text.size(); i++)
+		{
+			//Make sure character is in range
+			c = text[i];
+			if (c < header.start_char || c > header.end_char)
+				continue;
+
+			//Get pointer to glFont character
+			glfont_char = &header.chars[c - header.start_char];
+
+			//Get width and height
+			width += glfont_char->dx * header.tex_width;		
+		}
+
+		//Save width
+		size->first = (int)width;
+	}
+
+	//Begins text output with this font
+	void Begin (void);
+
+	//Template function to output a character array
+	template<class T> void DrawString (const T *text, float x,
+		float y)
+	{
+		const T *i;
+		GLFontChar *glfont_char;
+		float width, height;
+		
+		//Begin rendering quads
+		glBegin(GL_QUADS);
+		
+		//Loop through characters
+		for (i = text; *i != (T)'\0'; i++)
+		{
+			//Make sure character is in range
+			if (*i < header.start_char || *i > header.end_char)
+				continue;
+
+			//Get pointer to glFont character
+			glfont_char = &header.chars[*i - header.start_char];
+
+			//Get width and height
+			width = glfont_char->dx * header.tex_width;
+			height = glfont_char->dy * header.tex_height;
+			
+			//Specify vertices and texture coordinates
+			glTexCoord2f(glfont_char->tx1, glfont_char->ty1);
+			glVertex3f(x, y, 0.0F);
+			glTexCoord2f(glfont_char->tx1, glfont_char->ty2);
+			glVertex3f(x, y - height, 0.0F);
+			glTexCoord2f(glfont_char->tx2, glfont_char->ty2);
+			glVertex3f(x + width, y - height, 0.0F);
+			glTexCoord2f(glfont_char->tx2, glfont_char->ty1);
+			glVertex3f(x + width, y, 0.0F);
+		
+			//Move to next character
+			x += width;
+		}
+
+		//Stop rendering quads
+		glEnd();
+	}
+
+	//Template function to draw a std::basic_string
+	template<class T> void DrawString (
+		const std::basic_string<T> &text, float x, float y)
+	{
+		unsigned int i;
+		T c;
+		GLFontChar *glfont_char;
+		float width, height;
+		
+		//Begin rendering quads
+		glBegin(GL_QUADS);
+		
+		//Loop through characters
+		for (i = 0; i < text.size(); i++)
+		{
+			//Make sure character is in range
+			c = text[i];
+			if (c < header.start_char || c > header.end_char)
+				continue;
+
+			//Get pointer to glFont character
+			glfont_char = &header.chars[c - header.start_char];
+
+			//Get width and height
+			width = glfont_char->dx * header.tex_width;
+			height = glfont_char->dy * header.tex_height;
+			
+			//Specify vertices and texture coordinates
+			glTexCoord2f(glfont_char->tx1, glfont_char->ty1);
+			glVertex3f(x, y, 0.0F);
+			glTexCoord2f(glfont_char->tx1, glfont_char->ty2);
+			glVertex3f(x, y - height, 0.0F);
+			glTexCoord2f(glfont_char->tx2, glfont_char->ty2);
+			glVertex3f(x + width, y - height, 0.0F);
+			glTexCoord2f(glfont_char->tx2, glfont_char->ty1);
+			glVertex3f(x + width, y, 0.0F);
+		
+			//Move to next character
+			x += width;
+		}
+
+		//Stop rendering quads
+		glEnd();
+	}
+
+	//Template function to output a scaled character array
+	template<class T> void DrawString (const T *text, float scalar,
+		float x, float y)
+	{
+		const T *i;
+		GLFontChar *glfont_char;
+		float width, height;
+		
+		//Begin rendering quads
+		glBegin(GL_QUADS);
+		
+		//Loop through characters
+		for (i = text; *i != (T)'\0'; i++)
+		{
+			//Make sure character is in range
+			if (*i < header.start_char || *i > header.end_char)
+				continue;
+
+			//Get pointer to glFont character
+			glfont_char = &header.chars[*i - header.start_char];
+
+			//Get width and height
+			width = (glfont_char->dx * header.tex_width) * scalar;
+			height = (glfont_char->dy * header.tex_height) * scalar;
+			
+			//Specify vertices and texture coordinates
+			glTexCoord2f(glfont_char->tx1, glfont_char->ty1);
+			glVertex3f(x, y, 0.0F);
+			glTexCoord2f(glfont_char->tx1, glfont_char->ty2);
+			glVertex3f(x, y - height, 0.0F);
+			glTexCoord2f(glfont_char->tx2, glfont_char->ty2);
+			glVertex3f(x + width, y - height, 0.0F);
+			glTexCoord2f(glfont_char->tx2, glfont_char->ty1);
+			glVertex3f(x + width, y, 0.0F);
+		
+			//Move to next character
+			x += width;
+		}
+
+		//Stop rendering quads
+		glEnd();
+	}
+
+	//Template function to output a scaled std::basic_string
+	template<class T> void DrawString (
+		const std::basic_string<T> &text, float scalar, float x,
+		float y)
+	{
+		unsigned int i;
+		T c;
+		GLFontChar *glfont_char;
+		float width, height;
+		
+		//Begin rendering quads
+		glBegin(GL_QUADS);
+		
+		//Loop through characters
+		for (i = 0; i < text.size(); i++)
+		{
+			//Make sure character is in range
+			c = text[i];
+			if (c < header.start_char || c > header.end_char)
+				continue;
+
+			//Get pointer to glFont character
+			glfont_char = &header.chars[c - header.start_char];
+
+			//Get width and height
+			width = (glfont_char->dx * header.tex_width) * scalar;
+			height = (glfont_char->dy * header.tex_height) * scalar;
+			
+			//Specify vertices and texture coordinates
+			glTexCoord2f(glfont_char->tx1, glfont_char->ty1);
+			glVertex3f(x, y, 0.0F);
+			glTexCoord2f(glfont_char->tx1, glfont_char->ty2);
+			glVertex3f(x, y - height, 0.0F);
+			glTexCoord2f(glfont_char->tx2, glfont_char->ty2);
+			glVertex3f(x + width, y - height, 0.0F);
+			glTexCoord2f(glfont_char->tx2, glfont_char->ty1);
+			glVertex3f(x + width, y, 0.0F);
+		
+			//Move to next character
+			x += width;
+		}
+
+		//Stop rendering quads
+		glEnd();
+	}
+
+	//Template function to output a colored character array
+	template<class T> void DrawString (const T *text, float x,
+		float y, const float *top_color,
+		const float *bottom_color)
+	{
+		const T *i;
+		GLFontChar *glfont_char;
+		float width, height;
+		
+		//Begin rendering quads
+		glBegin(GL_QUADS);
+		
+		//Loop through characters
+		for (i = text; *i != '\0'; i++)
+		{
+			//Make sure character is in range
+			if (*i < header.start_char || *i > header.end_char)
+				continue;
+
+			//Get pointer to glFont character
+			glfont_char = &header.chars[*i - header.start_char];
+
+			//Get width and height
+			width = glfont_char->dx * header.tex_width;
+			height = glfont_char->dy * header.tex_height;
+			
+			//Specify colors, vertices, and texture coordinates
+			glColor3fv(top_color);
+			glTexCoord2f(glfont_char->tx1, glfont_char->ty1);
+			glVertex3f(x, y, 0.0F);
+			glTexCoord2f(glfont_char->tx2, glfont_char->ty1);
+			glVertex3f(x + width, y, 0.0F);
+			glColor3fv(bottom_color);
+			glTexCoord2f(glfont_char->tx2, glfont_char->ty2);
+			glVertex3f(x + width, y - height, 0.0F);
+			glTexCoord2f(glfont_char->tx1, glfont_char->ty2);
+			glVertex3f(x, y - height, 0.0F);		
+		
+			//Move to next character
+			x += width;
+		}
+
+		//Stop rendering quads
+		glEnd();
+	}
+
+	//Template function to output a colored std::basic_string
+	template<class T> void DrawString (
+		const std::basic_string<T> &text, float x, float y,
+		const float *top_color, const float *bottom_color)
+	{
+		unsigned int i;
+		T c;
+		GLFontChar *glfont_char;
+		float width, height;
+		
+		//Begin rendering quads
+		glBegin(GL_QUADS);
+		
+		//Loop through characters
+		for (i = 0; i < text.size(); i++)
+		{
+			//Make sure character is in range
+			c = text[i];
+			if (c < header.start_char || c > header.end_char)
+				continue;
+
+			//Get pointer to glFont character
+			glfont_char = &header.chars[c - header.start_char];
+
+			//Get width and height
+			width = glfont_char->dx * header.tex_width;
+			height = glfont_char->dy * header.tex_height;
+			
+			//Specify colors, vertices, and texture coordinates
+			glColor3fv(top_color);
+			glTexCoord2f(glfont_char->tx1, glfont_char->ty1);
+			glVertex3f(x, y, 0.0F);
+			glTexCoord2f(glfont_char->tx2, glfont_char->ty1);
+			glVertex3f(x + width, y, 0.0F);
+			glColor3fv(bottom_color);
+			glTexCoord2f(glfont_char->tx2, glfont_char->ty2);
+			glVertex3f(x + width, y - height, 0.0F);
+			glTexCoord2f(glfont_char->tx1, glfont_char->ty2);
+			glVertex3f(x, y - height, 0.0F);		
+		
+			//Move to next character
+			x += width;
+		}
+
+		//Stop rendering quads
+		glEnd();
+	}
+
+	//Template function to output a scaled, colored character array
+	template<class T> void DrawString (const T *text, float scalar,
+		float x, float y, const float *top_color,
+		const float *bottom_color)
+	{
+		const T *i;
+		GLFontChar *glfont_char;
+		float width, height;
+		
+		//Begin rendering quads
+		glBegin(GL_QUADS);
+		
+		//Loop through characters
+		for (i = text; *i != '\0'; i++)
+		{
+			//Make sure character is in range
+			if (*i < header.start_char || *i > header.end_char)
+				continue;
+
+			//Get pointer to glFont character
+			glfont_char = &header.chars[*i - header.start_char];
+
+			//Get width and height
+			width = (glfont_char->dx * header.tex_width) * scalar;
+			height = (glfont_char->dy * header.tex_height) * scalar;
+			
+			//Specify colors, vertices, and texture coordinates
+			glColor3fv(top_color);
+			glTexCoord2f(glfont_char->tx1, glfont_char->ty1);
+			glVertex3f(x, y, 0.0F);
+			glTexCoord2f(glfont_char->tx2, glfont_char->ty1);
+			glVertex3f(x + width, y, 0.0F);
+			glColor3fv(bottom_color);
+			glTexCoord2f(glfont_char->tx2, glfont_char->ty2);
+			glVertex3f(x + width, y - height, 0.0F);
+			glTexCoord2f(glfont_char->tx1, glfont_char->ty2);
+			glVertex3f(x, y - height, 0.0F);		
+		
+			//Move to next character
+			x += width;
+		}
+
+		//Stop rendering quads
+		glEnd();
+	}
+
+	//Template function to output a scaled, colored std::basic_string
+	template<class T> void DrawString (
+		const std::basic_string<T> &text, float scalar, float x,
+		float y, const float *top_color, const float *bottom_color)
+	{
+		unsigned int i;
+		T c;
+		GLFontChar *glfont_char;
+		float width, height;
+		
+		//Begin rendering quads
+		glBegin(GL_QUADS);
+		
+		//Loop through characters
+		for (i = 0; i < text.size(); i++)
+		{
+			//Make sure character is in range
+			c = text[i];
+			if (c < header.start_char || c > header.end_char)
+				continue;
+
+			//Get pointer to glFont character
+			glfont_char = &header.chars[c - header.start_char];
+
+			//Get width and height
+			width = (glfont_char->dx * header.tex_width) * scalar;
+			height = (glfont_char->dy * header.tex_height) * scalar;
+			
+			//Specify colors, vertices, and texture coordinates
+			glColor3fv(top_color);
+			glTexCoord2f(glfont_char->tx1, glfont_char->ty1);
+			glVertex3f(x, y, 0.0F);
+			glTexCoord2f(glfont_char->tx2, glfont_char->ty1);
+			glVertex3f(x + width, y, 0.0F);
+			glColor3fv(bottom_color);
+			glTexCoord2f(glfont_char->tx2, glfont_char->ty2);
+			glVertex3f(x + width, y - height, 0.0F);
+			glTexCoord2f(glfont_char->tx1, glfont_char->ty2);
+			glVertex3f(x, y - height, 0.0F);		
+		
+			//Move to next character
+			x += width;
+		}
+
+		//Stop rendering quads
+		glEnd();
+	}
+};
+
+//*******************************************************************
+
+#endif
+
+//End of file
+
