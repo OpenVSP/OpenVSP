249ac714d37c9442b92af64a6b653edb74a0329e
Rob McDonald <ramcdona@calpoly.edu>
2014-02-17 15:17:57 -0800

    Fix nasty FLTK interaction with Update

--- src/geom_core/Vehicle.cpp
+++ src/geom_core/Vehicle.cpp
@@ -223,7 +223,7 @@ void Vehicle::Renew()
 //==== Parm Changed ====//
 void Vehicle::ParmChanged( Parm* parm_ptr, int type )
 {
-    //UpdateBBox();  // This call creates recursive loop in UpdateBBox.
+    UpdateBBox();
     MessageMgr::getInstance().Send( "ScreenMgr", "UpdateAllScreens" );
 }
 
