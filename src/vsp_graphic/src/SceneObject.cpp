8a69a6e5bd2c607f83d62008b465167225c4da5d
Yue Han <yhan@Dart>
2014-03-20 13:34:10 -0400

    Added SceneObject class.  This class is the base class for all "Scene" objects.

---
+++ src/vsp_graphic/src/SceneObject.cpp
@@ -0,0 +1,48 @@
+#include "SceneObject.h"
+
+namespace VSPGraphic
+{
+SceneObject::SceneObject()
+{
+	_id = 0xFFFFFFFF;
+}
+SceneObject::~SceneObject()
+{
+}
+
+void SceneObject::predraw()
+{
+	if(_visible && _predrawFlag)
+	{
+		_predraw();
+	}
+}
+
+void SceneObject::draw()
+{
+	if(_visible)
+	{
+		_draw();
+	}
+}
+
+void SceneObject::setVisibility(bool visible)
+{
+	_visible = visible;
+}
+
+void SceneObject::enablePredraw(bool enable)
+{
+	_predrawFlag = enable;
+}
+
+unsigned int SceneObject::getID()
+{
+	return _id;
+}
+
+void SceneObject::setID(unsigned int id)
+{
+	_id = id;
+}
+}
