d6788fc730a0247d47faf8380036b75999dabe85
Yue Han <yhan@Dart>
2014-04-01 17:29:24 -0400

    Use dynamic_cast to determine the type of geometry in PickableGeom.

--- src/vsp_graphic/src/PickableGeom.cpp
+++ src/vsp_graphic/src/PickableGeom.cpp
@@ -3,6 +3,7 @@
 #include "VertexBuffer.h"
 #include "ElementBuffer.h"
 #include "ColorBuffer.h"
+#include "XSecEntity.h"
 
 namespace VSPGraphic
 {
@@ -21,24 +22,18 @@ void PickableGeom::update()
 
 void PickableGeom::_predraw()
 {
-	// Fix Me: Only Quad Geometries with Element Buffer are selectable. This takes the 
-	// assumption of Quad Geometries with Element Buffer is a Cross Section Geometry.
-	// The reason is to keep Render Engine as generic as possible, the engine
-	// does not track which renderable belongs to which geometry. In example, CFD
-	// Mesh contains multiple renderables to render a complete mesh. The engine is
-	// not able to id renderables that belong to this CFD Mesh.
+    // FIXME: Only Cross Section Geometries are selectable for now.
+    if(dynamic_cast<XSecEntity*>(_rSource))
+    {
+        ElementBuffer * eBuffer = _rSource->getEBuffer();
+        VertexBuffer * vBuffer = _rSource->getVBuffer();
 
-	if(_rSource->getPrimType() == Common::VSP_QUADS &&_rSource->getEBufferFlag())
-	{
-		ElementBuffer * eBuffer = _rSource->getEBuffer();
-		VertexBuffer * vBuffer = _rSource->getVBuffer();
-
-		_cIndexBuffer->bind();
-		eBuffer->bind();
-		vBuffer->drawElem(GL_QUADS, eBuffer->getElemSize(), (void*)0);
-		eBuffer->unbind();
-		_cIndexBuffer->unbind();
-	}
+        _cIndexBuffer->bind();
+        eBuffer->bind();
+        vBuffer->drawElem(GL_QUADS, eBuffer->getElemSize(), (void*)0);
+        eBuffer->unbind();
+        _cIndexBuffer->unbind();
+    }
 }
 
 void PickableGeom::_draw()
