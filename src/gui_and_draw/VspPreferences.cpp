90b18b226f23a3d8664f7c42931e7541dc3b9c96
Yue Han <yhan@Dart>
2014-05-19 11:09:32 -0400

    Added VspPreferences class.  This class read preference file and pass the data where they are needed.

---
+++ src/gui_and_draw/VspPreferences.cpp
@@ -0,0 +1,73 @@
+#include "VspPreferences.h"
+#include "XmlUtil.h"
+
+VspPreferences::VspPreferences()
+{
+    xmlDocPtr doc;
+    xmlNodePtr mat_root;
+
+    LIBXML_TEST_VERSION
+    xmlKeepBlanksDefault( 0 );
+
+    doc = xmlParseFile( "VspPreferences.xml" );
+
+    xmlNodePtr root = xmlDocGetRootElement( doc );
+    if ( root == NULL )
+    {
+        fprintf( stderr, "empty document\n" );
+        xmlFreeDoc( doc );
+    }
+
+    mat_root = XmlUtil::GetNode( root, "Vsp_Material_Defines", 0 );
+
+    int numOfMats = XmlUtil::GetNumNames( mat_root, "Material" );
+    for( int i = 0; i < numOfMats; i++ )
+    {
+        MaterialPref mat;
+
+        xmlNodePtr mat_node = XmlUtil::GetNode( mat_root, "Material", i );
+
+        xmlNodePtr nam_node = XmlUtil::GetNode( mat_node, "Name", 0 );
+        xmlNodePtr amb_node = XmlUtil::GetNode( mat_node, "Ambient", 0 );
+        xmlNodePtr dif_node = XmlUtil::GetNode( mat_node, "Diffuse", 0 );
+        xmlNodePtr spe_node = XmlUtil::GetNode( mat_node, "Specular", 0 );
+        xmlNodePtr emi_node = XmlUtil::GetNode( mat_node, "Emission", 0 );
+        xmlNodePtr shi_node = XmlUtil::GetNode( mat_node, "Shininess", 0 );
+        
+        mat.name = XmlUtil::ExtractString( nam_node );
+        mat.shininess = XmlUtil::ExtractDouble( shi_node );
+
+        XmlUtil::ExtractDoubleArray( amb_node, ',', &mat.ambi[0], 4 );
+        XmlUtil::ExtractDoubleArray( dif_node, ',', &mat.diff[0], 4 );
+        XmlUtil::ExtractDoubleArray( spe_node, ',', &mat.spec[0], 4 );
+        XmlUtil::ExtractDoubleArray( emi_node, ',', &mat.emis[0], 4 );
+        
+        m_MaterialPrefs.push_back( mat );
+    }
+}
+VspPreferences::~VspPreferences()
+{
+}
+
+bool VspPreferences::findMaterialPref( std::string name, VspPreferences::MaterialPref& out )
+{
+    for(int i = 0; i < (int) m_MaterialPrefs.size(); i++)
+    {
+        if( m_MaterialPrefs[i].name == name )
+        {
+            out = m_MaterialPrefs[i];
+            return true;
+        }
+    }
+    return false;
+}
+
+bool VspPreferences::findMaterialPref( int index, VspPreferences::MaterialPref& out )
+{
+    if( index >= 0 && index < m_MaterialPrefs.size() )
+    {
+        out = m_MaterialPrefs[index];
+        return true;
+    }
+    return false;
+}
