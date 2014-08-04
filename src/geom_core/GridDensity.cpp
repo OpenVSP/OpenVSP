87685ec5ea7249e40e9b98acb30ae0c3ab180cfd
Rob McDonald <ramcdona@calpoly.edu>
2014-08-03 20:20:28 -0700

    Introduce SimpleSources to actually interact with CFDMesh.
    Originally, Sources were copied to handle symmetry every time UpdateSources
    was called.  Since sources contained numerous Parms, this seemed to wreak
    havoc.
    
    So, SimpleSources are now the destination when Sources are copied for
    symmetry.  The parameters are placed in plain doubles, with no registration,
    ID, etc.  SimpleSources are used by CFDMesh and for visualization.  The
    original source class is used for interacting with the GUI, file I/O, etc.

--- src/geom_core/GridDensity.cpp
+++ src/geom_core/GridDensity.cpp
@@ -11,6 +11,7 @@
 #include "Geom.h"
 #include "Vehicle.h"
 #include "VehicleMgr.h"
+#include "ParmMgr.h"
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////
@@ -285,6 +286,560 @@ void BoxSource::SetNamedVal( string name, double val )
     else if ( name == "W2" )
     {
         m_WLoc2 = val;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+BaseSimpleSource::BaseSimpleSource()
+{
+    m_Name = "Source_Name";
+    m_Type = -1;
+
+    m_Len = 0.1;
+
+    m_Rad = 1.0;
+
+    m_MainSurfIndx = -1;
+    m_SurfIndx = -1;
+
+    m_DrawObjID = ParmMgr.GenerateID( 8 );
+}
+
+vector< vec3d > BaseSimpleSource::CreateSphere( double rad, const vec3d& loc )
+{
+    int i, j;
+    int num_lats = 8;
+    int num_longs = 8;
+
+    vector< vec3d > sphere;
+
+    for ( i = 0; i < num_lats; i++ )
+    {
+        double lat = PI * ( -0.5 + ( double )i / num_lats );
+        double z  = rad * sin( lat );
+        double zr = rad * cos( lat );
+
+        for ( j = 0; j < num_longs; j++ )
+        {
+            double lng = 2 * PI * ( double )j / num_longs;
+            double x = cos( lng ) * zr;
+            double y = sin( lng ) * zr;
+
+            sphere.push_back( vec3d( x + loc[0], y + loc[1], z + loc[2] ) );
+        }
+    }
+    return sphere;
+}
+
+void BaseSimpleSource::AdjustLen( double val )
+{
+    m_Len = m_Len * val;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+PointSimpleSource::PointSimpleSource()
+{
+    SetLoc( vec3d() );
+
+    m_ULoc = 0.0;
+
+    m_WLoc = 0.0;
+
+    m_Type = POINT_SOURCE;
+    m_Name = "Point_Name";
+
+    m_PointDO.m_GeomID = m_DrawObjID;
+    m_PointDO.m_Screen = DrawObj::VSP_MAIN_SCREEN;
+    m_PointDO.m_Type = DrawObj::VSP_LINE_LOOP;
+    m_PointDO.m_LineWidth = 1.0;
+    m_PointDO.m_LineColor = vec3d( 100.0 / 255, 100.0 / 255, 100.0 / 255 );
+}
+
+void PointSimpleSource::CopyFrom( BaseSource* s )
+{
+    m_Name = s->GetName();
+    m_Type = s->GetType();
+    m_Len = s->m_Len();
+    m_Rad = s->m_Rad();
+
+    m_MainSurfIndx = s->m_MainSurfIndx;
+    m_SurfIndx = s->m_SurfIndx;
+
+    m_OrigSourceID = s->GetID();
+
+    m_ULoc = ( ( PointSource* )s )->m_ULoc();
+    m_WLoc = ( ( PointSource* )s )->m_WLoc();
+}
+
+double PointSimpleSource::GetTargetLen( double base_len, vec3d &  pos )
+{
+    double dist2 = dist_squared( pos, m_Loc );
+
+    double radSquared = m_Rad * m_Rad;
+
+    if ( dist2 > radSquared )
+    {
+        return base_len;
+    }
+
+    double fract = dist2 / radSquared;
+
+    return ( m_Len + fract * ( base_len - m_Len  ) );
+}
+
+void PointSimpleSource::Update( Geom* geomPtr )
+{
+    vec3d p = geomPtr->GetUWPt( m_SurfIndx, m_ULoc, m_WLoc );
+
+    SetLoc( p );
+
+}
+
+void PointSimpleSource::LoadDrawObjs( vector< DrawObj* > & draw_obj_vec )
+{
+    m_PointDO.m_PntVec = CreateSphere( m_Rad, m_Loc );
+    draw_obj_vec.push_back( &m_PointDO );
+}
+
+void PointSimpleSource::Show( bool flag )
+{
+    m_PointDO.m_Visible = flag;
+}
+
+void PointSimpleSource::Highlight( bool flag )
+{
+    if( flag )
+    {
+        m_PointDO.m_LineColor = vec3d( 1, 100.0 / 255, 0 );
+    }
+    else
+    {
+        m_PointDO.m_LineColor = vec3d( 100.0 / 255, 100.0 / 255, 100.0 / 255 );
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+LineSimpleSource::LineSimpleSource()
+{
+    SetEndPnts( vec3d(), vec3d() );
+
+    m_Len2 = 0.1;
+
+    m_Rad2 = 1.0;
+
+    m_ULoc1 = 0.0;
+
+    m_WLoc1 = 0.0;
+
+    m_ULoc2 = 0.0;
+
+    m_WLoc2 = 0.0;
+
+    m_Type = LINE_SOURCE;
+    m_Name = "Line_Name";
+
+    m_LineDO1.m_GeomID = m_DrawObjID + "1";
+    m_LineDO1.m_Screen = DrawObj::VSP_MAIN_SCREEN;
+    m_LineDO1.m_Type = DrawObj::VSP_LINE_LOOP;
+    m_LineDO1.m_LineWidth = 1.0;
+    m_LineDO1.m_LineColor = vec3d( 100.0 / 255, 100.0 / 255, 100.0 / 255 );
+
+    m_LineDO2.m_GeomID = m_DrawObjID + "2";
+    m_LineDO2.m_Screen = DrawObj::VSP_MAIN_SCREEN;
+    m_LineDO2.m_Type = DrawObj::VSP_LINE_LOOP;
+    m_LineDO2.m_LineWidth = 1.0;
+    m_LineDO2.m_LineColor = vec3d( 100.0 / 255, 100.0 / 255, 100.0 / 255 );
+
+    m_LineDO3.m_GeomID = m_DrawObjID + "3";
+    m_LineDO3.m_Screen = DrawObj::VSP_MAIN_SCREEN;
+    m_LineDO3.m_Type = DrawObj::VSP_LINES;
+    m_LineDO3.m_LineWidth = 1.0;
+    m_LineDO3.m_LineColor = vec3d( 100.0 / 255, 100.0 / 255, 100.0 / 255 );
+}
+
+void LineSimpleSource::CopyFrom( BaseSource* s )
+{
+    m_Name = s->GetName();
+    m_Type = s->GetType();
+    m_Len = s->m_Len();
+    m_Rad = s->m_Rad();
+
+    m_MainSurfIndx = s->m_MainSurfIndx;
+    m_SurfIndx = s->m_SurfIndx;
+
+    m_OrigSourceID = s->GetID();
+
+    m_Len = ( ( LineSource* )s )->m_Len();
+    m_Len2 = ( ( LineSource* )s )->m_Len2();
+    m_Rad = ( ( LineSource* )s )->m_Rad();
+    m_Rad2 = ( ( LineSource* )s )->m_Rad2();
+
+    m_ULoc1 = ( ( LineSource* )s )->m_ULoc1();
+    m_WLoc1 = ( ( LineSource* )s )->m_WLoc1();
+
+    m_ULoc2 = ( ( LineSource* )s )->m_ULoc2();
+    m_WLoc2 = ( ( LineSource* )s )->m_WLoc2();
+}
+
+void LineSimpleSource::UpdateBBox()
+{
+    m_Box.Update( vec3d( m_Pnt1[0] + m_Rad, m_Pnt1[1] + m_Rad,  m_Pnt1[2] + m_Rad ) );
+    m_Box.Update( vec3d( m_Pnt1[0] - m_Rad, m_Pnt1[1] - m_Rad,  m_Pnt1[2] - m_Rad ) );
+    m_Box.Update( vec3d( m_Pnt2[0] + m_Rad2, m_Pnt2[1] + m_Rad2,  m_Pnt2[2] + m_Rad2 ) );
+    m_Box.Update( vec3d( m_Pnt2[0] - m_Rad2, m_Pnt2[1] - m_Rad2,  m_Pnt2[2] - m_Rad2 ) );
+}
+
+void LineSimpleSource::SetEndPnts( const vec3d & pnt1, const vec3d & pnt2 )
+{
+    m_Pnt1 = pnt1;
+    m_Pnt2 = pnt2;
+    m_Line = pnt2 - pnt1;
+    m_DotLine = max( 0.0000001, dot( m_Line, m_Line ) );
+    UpdateBBox();
+}
+
+void LineSimpleSource::AdjustLen( double val )
+{
+    m_Len = m_Len * val;
+    m_Len2 = m_Len2 * val;
+}
+
+double LineSimpleSource::GetTargetLen( double base_len, vec3d &  pos )
+{
+
+    double retlen = base_len;
+    if ( !m_Box.CheckPnt( pos[0], pos[1], pos[2] ) )
+    {
+        return retlen;
+    }
+
+    vec3d origVec = pos - m_Pnt1;
+    double numer = dot( m_Line, origVec );
+
+    double dist2;
+    double t = numer / m_DotLine;
+
+    if ( t <= 0 )
+    {
+        dist2 = dist_squared( m_Pnt1, pos );
+        if ( dist2 > m_RadSquared1 )
+        {
+            return base_len;
+        }
+        double fract = dist2 / m_RadSquared1;
+        retlen = m_Len + fract * ( base_len - m_Len  );
+    }
+    else if ( t >= 1 )
+    {
+        dist2 = dist_squared( m_Pnt2, pos );
+        if ( dist2 > m_RadSquared2 )
+        {
+            return base_len;
+        }
+        double fract = dist2 / m_RadSquared2;
+        retlen = m_Len2 + fract * ( base_len - m_Len2  );
+    }
+    else
+    {
+        vec3d proj = m_Pnt1 + m_Line * t;
+        dist2 = dist_squared( proj, pos );
+
+        double fract_rad = m_Rad + ( m_Rad2 - m_Rad ) * t;
+        double fract_rad_sqr = fract_rad * fract_rad;
+
+        if ( dist2 > fract_rad_sqr )
+        {
+            return base_len;
+        }
+
+        double fract_sqr = dist2 / fract_rad_sqr;
+        double fract_len = m_Len + ( m_Len2 - m_Len ) * t;
+
+        retlen = fract_len + fract_sqr * ( base_len - fract_len  );
+    }
+
+    if ( retlen > base_len )
+    {
+        printf( "Ret Len\n" );
+    }
+    else if ( retlen < m_Len && retlen < m_Len2 )
+    {
+        printf( "Ret Len\n" );
+    }
+
+
+    return retlen;
+}
+
+void LineSimpleSource::Update( Geom* geomPtr )
+{
+    m_Pnt1 = geomPtr->GetUWPt( m_SurfIndx, m_ULoc1, m_WLoc1 );
+    m_Pnt2 = geomPtr->GetUWPt( m_SurfIndx, m_ULoc2, m_WLoc2 );
+    SetEndPnts( m_Pnt1, m_Pnt2 );
+}
+
+void LineSimpleSource::LoadDrawObjs( vector< DrawObj* > & draw_obj_vec )
+{
+    m_LineDO1.m_PntVec = CreateSphere( m_Rad, m_Pnt1 );
+
+    m_LineDO2.m_PntVec = CreateSphere( m_Rad2, m_Pnt2 );
+
+    vector< vec3d > lines;
+    lines.push_back( vec3d( m_Pnt1[0], m_Pnt1[1], m_Pnt1[2] + m_Rad ) );
+    lines.push_back( vec3d( m_Pnt2[0], m_Pnt2[1], m_Pnt2[2] + m_Rad2 ) );
+    lines.push_back( vec3d( m_Pnt1[0], m_Pnt1[1], m_Pnt1[2] - m_Rad ) );
+    lines.push_back( vec3d( m_Pnt2[0], m_Pnt2[1], m_Pnt2[2] - m_Rad2 ) );
+    lines.push_back( vec3d( m_Pnt1[0], m_Pnt1[1] + m_Rad, m_Pnt1[2] ) );
+    lines.push_back( vec3d( m_Pnt2[0], m_Pnt2[1] + m_Rad2, m_Pnt2[2] ) );
+    lines.push_back( vec3d( m_Pnt1[0], m_Pnt1[1] - m_Rad, m_Pnt1[2] ) );
+    lines.push_back( vec3d( m_Pnt2[0], m_Pnt2[1] - m_Rad2, m_Pnt2[2] ) );
+    lines.push_back( vec3d( m_Pnt1[0] + m_Rad, m_Pnt1[1], m_Pnt1[2] ) );
+    lines.push_back( vec3d( m_Pnt2[0] + m_Rad2, m_Pnt2[1], m_Pnt2[2] ) );
+    lines.push_back( vec3d( m_Pnt1[0] - m_Rad, m_Pnt1[1], m_Pnt1[2] ) );
+    lines.push_back( vec3d( m_Pnt2[0] - m_Rad2, m_Pnt2[1], m_Pnt2[2] ) );
+    m_LineDO3.m_PntVec = lines;
+
+    draw_obj_vec.push_back( &m_LineDO1 );
+    draw_obj_vec.push_back( &m_LineDO2 );
+    draw_obj_vec.push_back( &m_LineDO3 );
+}
+
+void LineSimpleSource::Show( bool flag )
+{
+    m_LineDO1.m_Visible = flag;
+    m_LineDO2.m_Visible = flag;
+    m_LineDO3.m_Visible = flag;
+}
+
+void LineSimpleSource::Highlight( bool flag )
+{
+    if( flag )
+    {
+        m_LineDO1.m_LineColor = vec3d( 1, 100.0 / 255, 0 );
+        m_LineDO2.m_LineColor = vec3d( 1, 100.0 / 255, 0 );
+        m_LineDO3.m_LineColor = vec3d( 1, 100.0 / 255, 0 );
+    }
+    else
+    {
+        m_LineDO1.m_LineColor = vec3d( 100.0 / 255, 100.0 / 255, 100.0 / 255 );
+        m_LineDO2.m_LineColor = vec3d( 100.0 / 255, 100.0 / 255, 100.0 / 255 );
+        m_LineDO3.m_LineColor = vec3d( 100.0 / 255, 100.0 / 255, 100.0 / 255 );
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+BoxSimpleSource::BoxSimpleSource()
+{
+    m_Type = BOX_SOURCE;
+    m_Rad = 0;
+    m_Name = "Box_Name";
+
+    m_ULoc1 = 0.0;
+
+    m_WLoc1 = 0.0;
+
+    m_ULoc2 = 0.0;
+
+    m_WLoc2 = 0.0;
+
+    m_BoxDO1.m_GeomID = m_DrawObjID + "1";
+    m_BoxDO1.m_Screen = DrawObj::VSP_MAIN_SCREEN;
+    m_BoxDO1.m_Type = DrawObj::VSP_LINE_LOOP;
+    m_BoxDO1.m_LineWidth = 1.0;
+    m_BoxDO1.m_LineColor = vec3d( 100.0 / 255, 100.0 / 255, 100.0 / 255 );
+
+    m_BoxDO2.m_GeomID = m_DrawObjID + "2";
+    m_BoxDO2.m_Screen = DrawObj::VSP_MAIN_SCREEN;
+    m_BoxDO2.m_Type = DrawObj::VSP_LINE_LOOP;
+    m_BoxDO2.m_LineWidth = 1.0;
+    m_BoxDO2.m_LineColor = vec3d( 100.0 / 255, 100.0 / 255, 100.0 / 255 );
+
+    m_BoxDO3.m_GeomID = m_DrawObjID + "3";
+    m_BoxDO3.m_Screen = DrawObj::VSP_MAIN_SCREEN;
+    m_BoxDO3.m_Type = DrawObj::VSP_LINES;
+    m_BoxDO3.m_LineWidth = 1.0;
+    m_BoxDO3.m_LineColor = vec3d( 100.0 / 255, 100.0 / 255, 100.0 / 255 );
+}
+
+void BoxSimpleSource::ComputeCullPnts()
+{
+    m_CullMinPnt = m_MinPnt - vec3d( m_Rad, m_Rad, m_Rad );
+    m_CullMaxPnt = m_MaxPnt + vec3d( m_Rad, m_Rad, m_Rad );
+}
+
+
+void BoxSimpleSource::CopyFrom( BaseSource* s )
+{
+    m_Name = s->GetName();
+    m_Type = s->GetType();
+    m_Len = s->m_Len();
+    m_Rad = s->m_Rad();
+
+    m_MainSurfIndx = s->m_MainSurfIndx;
+    m_SurfIndx = s->m_SurfIndx;
+
+    m_OrigSourceID = s->GetID();
+
+    m_ULoc1 = ( ( BoxSource* )s )->m_ULoc1();
+    m_WLoc1 = ( ( BoxSource* )s )->m_WLoc1();
+    m_ULoc2 = ( ( BoxSource* )s )->m_ULoc2();
+    m_WLoc2 = ( ( BoxSource* )s )->m_WLoc2();
+}
+
+void BoxSimpleSource::SetMinMaxPnts( const vec3d & min_pnt, const vec3d & max_pnt )
+{
+    m_MinPnt = min_pnt;
+    m_MaxPnt = max_pnt;
+    ComputeCullPnts();
+    m_Box.Update( m_CullMinPnt );
+    m_Box.Update( m_CullMaxPnt );
+}
+
+void BoxSimpleSource::SetRad( double rad )
+{
+    m_Rad = rad;
+    ComputeCullPnts();
+    m_Box.Update( m_CullMinPnt );
+    m_Box.Update( m_CullMaxPnt );
+}
+
+
+double BoxSimpleSource::GetTargetLen( double base_len, vec3d &  pos )
+{
+    if ( pos[0] <= m_CullMinPnt[0] )
+    {
+        return base_len;
+    }
+    if ( pos[1] <= m_CullMinPnt[1] )
+    {
+        return base_len;
+    }
+    if ( pos[2] <= m_CullMinPnt[2] )
+    {
+        return base_len;
+    }
+    if ( pos[0] >= m_CullMaxPnt[0] )
+    {
+        return base_len;
+    }
+    if ( pos[1] >= m_CullMaxPnt[1] )
+    {
+        return base_len;
+    }
+    if ( pos[2] >= m_CullMaxPnt[2] )
+    {
+        return base_len;
+    }
+
+    if ( pos[0] > m_MinPnt[0] && pos[0] < m_MaxPnt[0] &&
+            pos[1] > m_MinPnt[1] && pos[1] < m_MaxPnt[1] &&
+            pos[2] > m_MinPnt[2] && pos[2] < m_MaxPnt[2] )
+    {
+        return m_Len;
+    }
+
+    double fract[3];
+
+    for ( int i = 0 ; i < 3 ; i++ )
+    {
+        fract[i] = 0;
+        if ( pos[i] < m_MinPnt[i] )
+        {
+            fract[i] = ( m_MinPnt[i] - pos[i] ) / m_Rad;
+        }
+        else if ( pos[i] > m_MaxPnt[i] )
+        {
+            fract[i] = ( pos[i] - m_MaxPnt[i] ) / m_Rad;
+        }
+    }
+
+    double max_fract = max( max( fract[0], fract[1] ), fract[2] );
+    return ( m_Len + max_fract * ( base_len - m_Len  ) );
+}
+
+void BoxSimpleSource::Update( Geom* geomPtr )
+{
+    BndBox box;
+    int num_segs = 8;
+    vector< vec3d > pVec;
+    for ( int i = 0 ; i < num_segs ; i++ )
+    {
+        double fu = ( double )i / ( double )( num_segs - 1 );
+        double u = m_ULoc1 + fu * ( m_ULoc2 - m_ULoc1 );
+        for ( int j = 0 ; j < num_segs ; j++ )
+        {
+            double fw = ( double )j / ( double )( num_segs - 1 );
+            double w = m_WLoc1 + fu * ( m_WLoc2 - m_WLoc1 );
+            vec3d p = geomPtr->GetUWPt( m_SurfIndx, u, w );
+            pVec.push_back( p );
+            box.Update( p );
+        }
+    }
+    m_Box = box;
+    vec3d p0 = box.GetCornerPnt( 0 );
+    vec3d p1 = box.GetCornerPnt( 7 );
+    SetMinMaxPnts( p0, p1 );
+
+}
+
+void BoxSimpleSource::LoadDrawObjs( vector< DrawObj* > & draw_obj_vec )
+{
+    vector< vec3d > loop1 , loop2, lines;
+
+    loop1.push_back( m_Box.GetCornerPnt( 0 ) );
+    loop1.push_back( m_Box.GetCornerPnt( 1 ) );
+    loop1.push_back( m_Box.GetCornerPnt( 3 ) );
+    loop1.push_back( m_Box.GetCornerPnt( 2 ) );
+    m_BoxDO1.m_PntVec = loop1;
+
+    loop2.push_back( m_Box.GetCornerPnt( 4 ) );
+    loop2.push_back( m_Box.GetCornerPnt( 5 ) );
+    loop2.push_back( m_Box.GetCornerPnt( 7 ) );
+    loop2.push_back( m_Box.GetCornerPnt( 6 ) );
+    m_BoxDO2.m_PntVec = loop2;
+
+    lines.push_back( m_Box.GetCornerPnt( 0 ) );
+    lines.push_back( m_Box.GetCornerPnt( 4 ) );
+    lines.push_back( m_Box.GetCornerPnt( 1 ) );
+    lines.push_back( m_Box.GetCornerPnt( 5 ) );
+    lines.push_back( m_Box.GetCornerPnt( 3 ) );
+    lines.push_back( m_Box.GetCornerPnt( 7 ) );
+    lines.push_back( m_Box.GetCornerPnt( 2 ) );
+    lines.push_back( m_Box.GetCornerPnt( 6 ) );
+    m_BoxDO3.m_PntVec = lines;
+
+    draw_obj_vec.push_back( &m_BoxDO1 );
+    draw_obj_vec.push_back( &m_BoxDO2 );
+    draw_obj_vec.push_back( &m_BoxDO3 );
+}
+
+void BoxSimpleSource::Show( bool flag )
+{
+    m_BoxDO1.m_Visible = flag;
+    m_BoxDO2.m_Visible = flag;
+    m_BoxDO3.m_Visible = flag;
+}
+
+void BoxSimpleSource::Highlight( bool flag )
+{
+    if( flag )
+    {
+        m_BoxDO1.m_LineColor = vec3d( 1, 100.0 / 255, 0 );
+        m_BoxDO2.m_LineColor = vec3d( 1, 100.0 / 255, 0 );
+        m_BoxDO3.m_LineColor = vec3d( 1, 100.0 / 255, 0 );
+    }
+    else
+    {
+        m_BoxDO1.m_LineColor = vec3d( 100.0 / 255, 100.0 / 255, 100.0 / 255 );
+        m_BoxDO2.m_LineColor = vec3d( 100.0 / 255, 100.0 / 255, 100.0 / 255 );
+        m_BoxDO3.m_LineColor = vec3d( 100.0 / 255, 100.0 / 255, 100.0 / 255 );
     }
 }
 
--- src/geom_core/GridDensity.h
+++ src/geom_core/GridDensity.h
@@ -183,6 +183,189 @@ protected:
 };
 
 //////////////////////////////////////////////////////////////////////
+
+class BaseSimpleSource
+{
+public:
+
+	BaseSimpleSource();
+    virtual ~BaseSimpleSource()   {};
+
+    virtual void SetName( const string str )
+    {
+        m_Name = str;
+    }
+    virtual string GetName()
+    {
+        return m_Name;
+    }
+    virtual void AdjustLen( double val  );
+
+    virtual double GetTargetLen( double base_len, vec3d &  pos ) = 0;
+
+    virtual int GetType()
+    {
+        return m_Type;
+    }
+
+    virtual void Draw()                                             {}
+
+    virtual void Update( Geom* geomPtr )                            {}
+
+    virtual void CopyFrom( BaseSource* s ) = 0;
+
+    virtual vector< vec3d > CreateSphere( double rad, const vec3d& loc );
+
+    virtual void LoadDrawObjs( vector< DrawObj* > & draw_obj_vec ) = 0;
+    virtual void Show( bool flag ) = 0;
+    virtual void Highlight( bool flag ) = 0;
+
+    enum { POINT_SOURCE, LINE_SOURCE, BOX_SOURCE, NUM_SOURCE_TYPES, };
+
+    double m_Len;
+    double m_Rad;
+
+    int m_MainSurfIndx;
+    int m_SurfIndx;
+    string m_OrigSourceID;
+
+protected:
+
+    string m_Name;
+
+    int m_Type;
+
+    BndBox m_Box;
+
+    string m_DrawObjID;
+};
+
+//////////////////////////////////////////////////////////////////////
+class PointSimpleSource : public BaseSimpleSource
+{
+public:
+	PointSimpleSource();
+    virtual ~PointSimpleSource()      {}
+
+    void SetLoc( const vec3d & loc )
+    {
+        m_Loc = loc;
+    }
+
+    double GetTargetLen( double base_len, vec3d &  pos );
+
+    virtual void Update( Geom* geomPtr );
+
+    virtual void CopyFrom( BaseSource* s );
+
+    virtual void LoadDrawObjs( vector< DrawObj* > & draw_obj_vec );
+    virtual void Show( bool flag );
+    virtual void Highlight( bool flag );
+
+    double m_ULoc;
+    double m_WLoc;
+
+protected:
+
+    vec3d m_Loc;
+
+    DrawObj m_PointDO;
+};
+
+//////////////////////////////////////////////////////////////////////
+class LineSimpleSource : public BaseSimpleSource
+{
+public:
+	LineSimpleSource();
+    virtual ~LineSimpleSource()       {}
+
+    void SetEndPnts( const vec3d & pnt1, const vec3d & pnt2 );
+    void UpdateBBox();
+
+    void SetNamedVal( string name, double val );
+
+    virtual void AdjustLen( double val  );
+
+    double GetTargetLen( double base_len, vec3d &  pos );
+
+    virtual void Update( Geom* geomPtr );
+
+    virtual void CopyFrom( BaseSource* s );
+
+    virtual void LoadDrawObjs( vector< DrawObj* > & draw_obj_vec );
+    virtual void Show( bool flag );
+    virtual void Highlight( bool flag );
+
+    double m_ULoc1;
+    double m_WLoc1;
+
+    double m_ULoc2;
+    double m_WLoc2;
+
+    double m_Len2;
+    double m_Rad2;
+
+protected:
+
+    vec3d m_Pnt1;
+    vec3d m_Pnt2;
+
+    double m_RadSquared1;
+    double m_RadSquared2;
+
+    vec3d m_Line;                       // m_Pnt2 - m_Pnt1
+    double m_DotLine;                   // dot( m_Line, m_Line )
+
+    DrawObj m_LineDO1;
+    DrawObj m_LineDO2;
+    DrawObj m_LineDO3;
+};
+
+//////////////////////////////////////////////////////////////////////
+class BoxSimpleSource : public BaseSimpleSource
+{
+public:
+	BoxSimpleSource();
+    virtual ~BoxSimpleSource()        {};
+
+    void SetMinMaxPnts( const vec3d & min_pnt, const vec3d & max_pnt );
+
+    void ComputeCullPnts();
+
+    double GetTargetLen( double base_len, vec3d &  pos );
+
+    void Update( Geom* geomPtr );
+
+    void SetRad( double rad );
+
+    void SetNamedVal( string name, double val );
+
+    double m_ULoc1;
+    double m_WLoc1;
+
+    double m_ULoc2;
+    double m_WLoc2;
+
+    virtual void CopyFrom( BaseSource* s );
+
+    virtual void LoadDrawObjs( vector< DrawObj* > & draw_obj_vec );
+    virtual void Show( bool flag );
+    virtual void Highlight( bool flag );
+
+protected:
+
+    vec3d m_MinPnt;
+    vec3d m_MaxPnt;
+
+    vec3d m_CullMinPnt;
+    vec3d m_CullMaxPnt;
+
+    DrawObj m_BoxDO1;
+    DrawObj m_BoxDO2;
+    DrawObj m_BoxDO3;
+};
+
+//////////////////////////////////////////////////////////////////////
 class GridDensity : public ParmContainer
 {
 public:
