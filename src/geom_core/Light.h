91b63e1ec0609b978a132aa39941ff31a880c60e
Rob McDonald <ramcdona@calpoly.edu>
2014-08-15 18:24:43 -0700

    Clean up useless comments.

--- src/geom_core/Light.h
+++ src/geom_core/Light.h
@@ -4,19 +4,10 @@
 #include "ParmContainer.h"
 #include "Parm.h"
 
-/*!
-* Light info.
-*/
 class Light : public ParmContainer
 {
 public:
-    /*!
-    * Construct a light.
-    */
     Light();
-    /*!
-    * Destructor.
-    */
     virtual ~Light();
 
 public:
@@ -26,35 +17,14 @@ public:
     virtual void ParmChanged( Parm* parm_ptr, int type );
 
 public:
-    /*!
-    * Is light enabled?
-    */
     BoolParm m_Active;
 
-    /*!
-    * Light position on x-axis.
-    */
     Parm m_X;
-    /*!
-    * Light position on y-axis.
-    */
     Parm m_Y;
-    /*!
-    * Light position on z-axis.
-    */
     Parm m_Z;
 
-    /*!
-    * Ambient value of this light.
-    */
     Parm m_Amb;
-    /*!
-    * Diffuse value of this light.
-    */
     Parm m_Diff;
-    /*!
-    * Specular value of this light.
-    */
     Parm m_Spec;
 };
 #endif
