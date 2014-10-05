129c9e100abeca6cc8e76b36221ccf698de3b9a0
Rob McDonald <ramcdona@calpoly.edu>
2014-10-05 08:46:26 -0700

    Place capping parameters in their own group.

--- src/geom_core/Geom.cpp
+++ src/geom_core/Geom.cpp
@@ -642,31 +642,31 @@ Geom::Geom( Vehicle* vehicle_ptr ) : GeomXForm( vehicle_ptr )
     m_ShellFlag.Init( "Shell_Flag", "Mass_Props", this, false, 0, 1 );
 
     // End Cap Options
-    m_CapUMinOption.Init("CapUMinOption", m_Name, this, VspSurf::NO_END_CAP, VspSurf::NO_END_CAP, VspSurf::NUM_END_CAP_OPTIONS-1);
+    m_CapUMinOption.Init("CapUMinOption", "EndCap", this, VspSurf::NO_END_CAP, VspSurf::NO_END_CAP, VspSurf::NUM_END_CAP_OPTIONS-1);
     m_CapUMinOption.SetDescript("Type of End Cap on UMin end");
 
-    m_CapUMinTess.Init("CapUMinTess", m_Name, this, 3, 3, 51);
+    m_CapUMinTess.Init("CapUMinTess", "EndCap", this, 3, 3, 51);
     m_CapUMinTess.SetDescript("Number of tessellated curves on UMin end");
     m_CapUMinTess.SetMultShift(2, 1);
 
-    m_CapUMaxOption.Init("CapUMaxOption",   m_Name, this, VspSurf::NO_END_CAP, VspSurf::NO_END_CAP, VspSurf::NUM_END_CAP_OPTIONS-1);
+    m_CapUMaxOption.Init("CapUMaxOption", "EndCap", this, VspSurf::NO_END_CAP, VspSurf::NO_END_CAP, VspSurf::NUM_END_CAP_OPTIONS-1);
     m_CapUMaxOption.SetDescript("Type of End Cap on UMax end");
 
-    m_CapUMaxTess.Init("CapUMaxTess", m_Name, this, 3, 3, 51);
+    m_CapUMaxTess.Init("CapUMaxTess", "EndCap", this, 3, 3, 51);
     m_CapUMaxTess.SetDescript("Number of tessellated curves on UMax end");
     m_CapUMaxTess.SetMultShift(2, 1);
 
-    m_CapWMinOption.Init("CapWMinOption", m_Name, this, VspSurf::NO_END_CAP, VspSurf::NO_END_CAP, VspSurf::NUM_END_CAP_OPTIONS-1);
+    m_CapWMinOption.Init("CapWMinOption", "EndCap", this, VspSurf::NO_END_CAP, VspSurf::NO_END_CAP, VspSurf::NUM_END_CAP_OPTIONS-1);
     m_CapWMinOption.SetDescript("Type of End Cap on WMin end");
 
-    m_CapWMinTess.Init("CapWMinTess", m_Name, this, 3, 3, 51);
+    m_CapWMinTess.Init("CapWMinTess", "EndCap", this, 3, 3, 51);
     m_CapWMinTess.SetDescript("Number of tessellated curves on WMin end");
     m_CapWMinTess.SetMultShift(2, 1);
 
-    m_CapWMaxOption.Init("CapWMaxOption",   m_Name, this, VspSurf::NO_END_CAP, VspSurf::NO_END_CAP, VspSurf::NUM_END_CAP_OPTIONS-1);
+    m_CapWMaxOption.Init("CapWMaxOption", "EndCap", this, VspSurf::NO_END_CAP, VspSurf::NO_END_CAP, VspSurf::NUM_END_CAP_OPTIONS-1);
     m_CapWMaxOption.SetDescript("Type of End Cap on WMax end");
 
-    m_CapWMaxTess.Init("CapWMaxTess", m_Name, this, 3, 3, 51);
+    m_CapWMaxTess.Init("CapWMaxTess", "EndCap", this, 3, 3, 51);
     m_CapWMaxTess.SetDescript("Number of tessellated curves on WMax end");
     m_CapWMaxTess.SetMultShift(2, 1);
 
