4b8c5dd8967336903d868db77d17a8baa281a718
Rob McDonald <ramcdona@calpoly.edu>
2014-10-06 23:42:26 -0700

    Add barebones scriptonly executable target.

---
+++ src/vsp/scriptonly_main.cpp
@@ -0,0 +1,19 @@
+//
+// This file is released under the terms of the NASA Open Source Agreement (NOSA)
+// version 1.3 as detailed in the LICENSE file which accompanies this software.
+//
+
+#include "VehicleMgr.h"
+#include "common.h"
+
+//========================================================//
+//========================================================//
+//========================= Main =========================//
+
+int main( int argc, char** argv )
+{
+    //==== Get Vehicle Ptr ====//
+    Vehicle* vPtr = VehicleMgr.GetVehicle();
+
+    batchMode( argc, argv, vPtr );
+}
