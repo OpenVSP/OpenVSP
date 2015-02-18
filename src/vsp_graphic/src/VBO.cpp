9559513f3c61d17a8307a367b3699e5baf128b96
Rob McDonald <ramcdona@calpoly.edu>
2015-02-17 20:43:46 -0800

    Improve error reporting if OpenGL VBO support is not present.

--- src/vsp_graphic/src/VBO.cpp
+++ src/vsp_graphic/src/VBO.cpp
@@ -16,6 +16,18 @@ VBO::VBO( GLenum type )
     if ( !GL_ARB_vertex_buffer_object )
     {
         _support = false;
+        const GLubyte* str = glGetString( GL_VERSION );
+        printf("Current driver does not support Vertex Buffer Object.\n");
+        printf("OpenGL version reports: %s\n", str );
+        assert( false ); // Current driver does not support Vertex Buffer Object.
+        return;
+    }
+
+    if ( !glGenBuffers )
+    {
+        const GLubyte* str = glGetString( GL_VERSION );
+        printf("Current driver does not support Vertex Buffer Object.\n");
+        printf("OpenGL version reports: %s\n", str );
         assert( false ); // Current driver does not support Vertex Buffer Object.
         return;
     }
