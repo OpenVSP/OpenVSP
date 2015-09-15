1a0e6f6b942c6ee2176433346bfc16124fa698b5
Rob McDonald <ramcdona@calpoly.edu>
2015-09-15 15:22:26 -0700

    Change tolerance to scientific notation for easier searching.

--- src/cfd_mesh/SurfPatch.cpp
+++ src/cfd_mesh/SurfPatch.cpp
@@ -485,7 +485,7 @@ void SurfPatch::AddTVal( double t, vector< double > & t_vals )
     bool add_flag = true;
     for ( int i = 0 ; i < ( int )t_vals.size() ; i++ )
     {
-        if ( fabs( t - t_vals[i] ) < 0.000001 )
+        if ( fabs( t - t_vals[i] ) < 1.0e-6 )
         {
             add_flag = false;
         }
