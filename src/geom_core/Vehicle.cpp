76c9cd14819791b685c121796807413af1afffac
Rob McDonald <ramcdona@calpoly.edu>
2015-10-08 13:15:10 -0700

    Change STEP to split surfaces by default.

--- src/geom_core/Vehicle.cpp
+++ src/geom_core/Vehicle.cpp
@@ -42,7 +42,7 @@ Vehicle::Vehicle()
 {
     m_STEPLenUnit.Init( "LenUnit", "STEPSettings", this, STEPutil::LenEnum::u_FT, STEPutil::LenEnum::u_MM, STEPutil::LenEnum::u_YD );
     m_STEPTol.Init( "Tolerance", "STEPSettings", this, 1e-6, 1e-12, 1e12 );
-    m_STEPSplitSurfs.Init( "SplitSurfs", "STEPSettings", this, false, 0, 1 );
+    m_STEPSplitSurfs.Init( "SplitSurfs", "STEPSettings", this, true, 0, 1 );
     m_STEPMergePoints.Init( "MergePoints", "STEPSettings", this, true, 0, 1 );
     m_STEPToCubic.Init( "ToCubic", "STEPSettings", this, false, 0, 1 );
     m_STEPToCubicTol.Init( "ToCubicTol", "STEPSettings", this, 1e-6, 1e-12, 1e12 );
@@ -134,7 +134,7 @@ void Vehicle::Init()
 
     m_STEPLenUnit.Set( STEPutil::LenEnum::u_FT );
     m_STEPTol.Set( 1e-6 );
-    m_STEPSplitSurfs.Set( false );
+    m_STEPSplitSurfs.Set( true );
     m_STEPMergePoints.Set( true );
     m_STEPToCubic.Set( false );
     m_STEPToCubicTol.Set( 1e-6 );
