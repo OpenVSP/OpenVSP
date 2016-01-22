970e1a011a8db817277a041c74b9d2b5f3936961
Rob McDonald <ramcdona@calpoly.edu>
2016-01-22 09:44:27 -0800

    Add stb image loader to build system.

--- src/external/stb/dummy.cpp
+++ src/external/stb/dummy.cpp
@@ -2,3 +2,9 @@
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_ONLY_JPEG
+#define STBI_ONLY_PNG
+#define STBI_ONLY_TGA
+#include "stb_image.h"
