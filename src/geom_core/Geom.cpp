7eedb9feb061a69720bf09f6524e27726f6cc2e3
Rob McDonald <ramcdona@calpoly.edu>
2016-05-06 14:55:50 -0700

    Protect against NULL pointer passed to ParmChanged.
    NULL is explicitly passed in the API SetXSecWidthHeight.  So it can be
    called from the API or Custom Components.

--- src/geom_core/Geom.cpp
+++ src/geom_core/Geom.cpp
@@ -126,7 +126,10 @@ void GeomBase::ParmChanged( Parm* parm_ptr, int type )
     //==== Check For Interactive Collision Dectection When Alt Key Is Pressed ====//
     if ( type == Parm::SET_FROM_DEVICE )
     {
-        m_Vehicle->GetSnapToPtr()->PreventCollision( GetID(), parm_ptr->GetID() );
+        if ( parm_ptr )
+        {
+            m_Vehicle->GetSnapToPtr()->PreventCollision( GetID(), parm_ptr->GetID() );
+        }
     }
 
     Update();
