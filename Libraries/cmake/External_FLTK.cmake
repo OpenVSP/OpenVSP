6c47cfcea90dc041d230ac1815abe1454333e9cb
Rob McDonald <rob.a.mcdonald@gmail.com>
2016-12-06 09:00:04 -0800

    Update FLTK to 1.3.4-1
    Update to latest stable release of FLTK and clean up build system.

--- Libraries/cmake/External_FLTK.cmake
+++ Libraries/cmake/External_FLTK.cmake
@@ -1,24 +1,20 @@
 CMAKE_MINIMUM_REQUIRED( VERSION 2.8 )
 
 IF( WIN32 )
-#	SET( FLTK_PATCH PATCH_COMMAND patch -p1 --binary < ${CMAKE_SOURCE_DIR}/0003-Update-built-in-JPEG-CMake-to-install-headers.patch )
-	SET( FLTK_PATCH PATCH_COMMAND ${CMAKE_COMMAND} -E copy_if_different
-		"${CMAKE_CURRENT_SOURCE_DIR}/FLTK_JPEG_CMakeLists.txt"
-		"<SOURCE_DIR>/jpeg/CMakeLists.txt" )
+#	SET( FLTK_PATCH PATCH_COMMAND patch -p1 --binary < ${CMAKE_SOURCE_DIR}/foo.patch )
+#	SET( FLTK_PATCH PATCH_COMMAND ${CMAKE_COMMAND} -E copy_if_different
+#		"${CMAKE_CURRENT_SOURCE_DIR}/FLTK_JPEG_CMakeLists.txt"
+#		"<SOURCE_DIR>/jpeg/CMakeLists.txt" )
 
 ELSE()
-	SET( FLTK_PATCH PATCH_COMMAND patch -p1 < ${CMAKE_SOURCE_DIR}/0002-Fix-LLVM-libc-errors-with-static_cast-CGFloat.patch )
+#	SET( FLTK_PATCH PATCH_COMMAND patch -p1 < ${CMAKE_SOURCE_DIR}/foo.patch )
 ENDIF()
-
-# COMMAND patch -p1 < ${CMAKE_SOURCE_DIR}/0001-Changes-to-CMakeLists.txt-to-build-on-llvm-on-MacOS.patch
-# COMMAND patch -p1 < ${CMAKE_SOURCE_DIR}/0003-Update-built-in-JPEG-CMake-to-install-headers.patch
-# COMMAND patch -p1 < ${CMAKE_SOURCE_DIR}/0004-Fix-friend-declaration-error.patch )
 
 #    ${FLTK_PATCH}
 
 ExternalProject_Add( FLTK
 
-	URL ${CMAKE_SOURCE_DIR}/fltk-1.3.x-r10934.tar.gz
+	URL ${CMAKE_SOURCE_DIR}/fltk-1.3.4-1-source.tar.gz
 	CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
 		-DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
 		-DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
