63b21ca5c77726485cd87e73775247485dd86b4d
Nick Brake <nick.brake@esaero.com>
2017-02-17 12:14:43 -0800

    Adds comments for DegenSubSurf struct documentation

--- src/geom_core/DegenGeom.h
+++ src/geom_core/DegenGeom.h
@@ -84,10 +84,10 @@ typedef struct
 
 typedef struct
 {
-    string                      name;       //!
-    int                         testType;   //!
-    string                      typeName;   //! used to identify the design intent of the surface
-    vsp::SUBSURF_TYPE           typeId;     //!
+    string                      name;       //! name displayed in the geom GUI
+    int                         testType;   //! "test" for subsurfaces to define which side of the boundary the subsurface is on (commonly used for LINE subsurface types)
+    string                      typeName;   //! used to identify the design intent of the surface (see vsp::SUBSURF_TYPE in APIDefines.h and std::string SubSurface::GetTypeName( int type ) for more info)
+    vsp::SUBSURF_TYPE           typeId;     //! enumeration for the typeName; vsp::SUBSURF_TYPE
     vector< double >            u;          //!
     vector< double >            w;          //!
 } DegenSubSurf;
