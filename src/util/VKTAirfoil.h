7a3c8fbaa4dd2b0681a26f753e05d935e977b8fa
fernape <fernando.apesteguia@gmail.com>
2017-07-22 17:43:39 +0200

    Fix caseness in include directives

--- src/util/VKTAirfoil.h
+++ src/util/VKTAirfoil.h
@@ -11,7 +11,7 @@
 #if !defined(VKTAIRFOIL__INCLUDED_)
 #define VKTAIRFOIL__INCLUDED_
 
-#include "vec3d.h"
+#include "Vec3d.h"
 
 vec3d vkt_airfoil_point( double theta, double epsilon, double kappa, double tau );
 
