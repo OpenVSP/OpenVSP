80d8cd7822e2214ee205e9bbf93ebce602b8f83d
Justin Gravett <justin.gravett@esaero.com>
2017-07-25 16:16:53 -0700

    BUGFIX: Removes Ability to Split Endpoints of m_ISegDeque
    This fixes a bug that was occuring for constant W SSLine SubSurfaces
    on PodGeoms where border curve splits were identified and made on
    the endpoints of the SubSurface. This commit adds a check to
    ISegChain::AddBorderSplit so that the endpoints of the total ISegDeque
    can't be split.

--- src/cfd_mesh/ISegChain.cpp
+++ src/cfd_mesh/ISegChain.cpp
@@ -862,7 +862,8 @@ void ISegChain::AddBorderSplit( IPnt* ip, Puw* uw )
         vec2d iuw0 = m_ISegDeque[i]->m_IPnt[0]->GetPuw( surfPtr )->m_UW;
         vec2d iuw1 = m_ISegDeque[i]->m_IPnt[1]->GetPuw( surfPtr )->m_UW;
         double u = proj_pnt_on_line_u( iuw0, iuw1, uw->m_UW );
-        if ( u >= 0.0 && u <= 1.0 )
+        if ( ( u >= 0.0 && u <= 1.0 ) && 
+            ( !( u == 1.0 && i == m_ISegDeque.size() - 1 ) && !( u == 0.0 && i == 0 ) ) ) // Do not split endpoints of m_ISegDeque
         {
             vec2d proj = iuw0 + ( iuw1 - iuw0 ) * u;
 
