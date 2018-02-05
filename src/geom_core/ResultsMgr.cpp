e863cbcb43ba6dd3e9cc4c887eb55743718c148d
Justin Gravett <justin.gravett@esaero.com>
2018-02-05 13:50:00 -0800

    Fixes Compiler Warning in Results::WriteCSVFile
    m_Timestamp is printed to file as a long long int to match the time_t
    data type.

--- src/geom_core/ResultsMgr.cpp
+++ src/geom_core/ResultsMgr.cpp
@@ -235,7 +235,7 @@ void Results::WriteCSVFile( FILE* fid )
     {
 
         fprintf( fid, "Results_Name,%s\n", m_Name.c_str() );
-        fprintf( fid, "Results_Timestamp,%ld\n", m_Timestamp );
+        fprintf( fid, "Results_Timestamp,%lld\n", m_Timestamp );
         fprintf( fid, "Results_Date,%d,%d,%d\n", m_Month, m_Day, m_Year );
         fprintf( fid, "Results_Time,%d,%d,%d\n", m_Hour, m_Min, m_Sec );
 
