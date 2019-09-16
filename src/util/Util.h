19d640a1fae967c00442cd74f12b9f969021a9f2
Rob McDonald <rob.mcdonald@uber.com>
2019-09-16 16:24:38 -0700

    #define arguments should have ()

--- src/util/Util.h
+++ src/util/Util.h
@@ -17,7 +17,7 @@ using namespace std;
 #ifndef MIN
 #define MIN(x,y) (((x) < (y)) ? (x):(y))
 #endif
-#define DEG2RAD(x) ( x*0.017453293 )
+#define DEG2RAD(x) ( (x)*0.017453293 )
 
 #define INTERP_PARM( ptr_a, ptr_b, f, parm_name ) ((parm_name) = linterp( (ptr_a)->parm_name(), (ptr_b)->parm_name(), (f) ))
 
