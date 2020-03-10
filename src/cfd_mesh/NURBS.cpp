e10ba321b3bf44452fc7c0d9559c30a2a93c81a8
Justin Gravett <justin.gravett@esaero.com>
2020-03-10 09:52:09 -0700

    NURBS Boiler Plate: Structures & Functions for B-Spline Surfs & Curves

---
+++ src/cfd_mesh/NURBS.cpp
@@ -0,0 +1,9 @@
+//
+// This file is released under the terms of the NASA Open Source Agreement (NOSA)
+// version 1.3 as detailed in the LICENSE file which accompanies this software.
+//
+
+// NURBS (Non-Uniform Rational B-Spline)
+//
+//////////////////////////////////////////////////////////////////////
+
---
+++ src/cfd_mesh/NURBS.h
@@ -0,0 +1,21 @@
+//
+// This file is released under the terms of the NASA Open Source Agreement (NOSA)
+// version 1.3 as detailed in the LICENSE file which accompanies this software.
+//
+
+//////////////////////////////////////////////////////////////////////
+// NURBS.h
+// Justin Gravett (ESAero)
+//
+// Data structures for defining NURBS (Non-Uniform Rational B-Spline) curves, 
+// surfaces, and loops (chains) and functions to extract NURBS data from OpenVSP 
+// Bezier surfaces and export to various CAD formats. 
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(NURBS_NURBS__INCLUDED_)
+#define NURBS_NURBS__INCLUDED_
+
+
+
+#endif
