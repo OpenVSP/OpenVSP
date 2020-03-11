8c88412789f53f6688b74aab8a2fd39887996997
Justin Gravett <justin.gravett@esaero.com>
2020-03-11 14:01:58 -0700

    Defines the NURBS Curve Class
    This class is used to represent NURBS curves for CAD export. See
    the header file for additional details

--- src/cfd_mesh/NURBS.cpp
+++ src/cfd_mesh/NURBS.cpp
@@ -7,3 +7,79 @@
 //
 //////////////////////////////////////////////////////////////////////
 
+#include "NURBS.h"
+
+
+//////////////////////////////////////////////////////
+//================= NURBS_Curve ====================//
+//////////////////////////////////////////////////////
+
+NURBS_Curve::NURBS_Curve()
+{
+    m_BorderFlag = false;
+    m_InternalFlag = false;
+    m_SubSurfFlag = false;
+    m_StructIntersectFlag = false;
+    m_SurfA_ID = -1;
+    m_SurfB_ID = -1;
+    m_MergeTol = 0;
+    m_Deg = 1;
+    m_STEP_Start_Vert = NULL;
+    m_STEP_End_Vert = NULL;
+    m_STEP_Edge = NULL;
+    m_IGES_Edge = NULL;
+    m_SurfIntersectType = vsp::CFD_NORMAL;
+    m_BBox = BndBox();
+}
+
+void NURBS_Curve::InitNURBSCurve( SCurve curve )
+{
+    Bezier_curve xyzcrvA = curve.GetUWCrv();
+    xyzcrvA.TessAdaptXYZ( *curve.GetSurf(), m_PntVec, 5e-3, 16 );
+
+    m_BBox = curve.GetSurf()->GetBBox();
+
+    m_MergeTol = m_BBox.DiagDist() * 1.0e-10;
+
+    if ( m_MergeTol < 1.0e-10 )
+    {
+        m_MergeTol = 1.0e-10;
+    }
+}
+
+void NURBS_Curve::WriteIGESEdge( IGESutil* iges )
+{
+    m_IGES_Edge = new DLL_IGES_ENTITY_126( iges->MakeCurve( m_PntVec, m_Deg ) );
+}
+
+void NURBS_Curve::WriteSTEPEdge( STEPutil* step, bool mergepnts )
+{
+    // Identify the start and end vertex points
+    m_STEP_Start_Vert = step->MakeVertex( m_PntVec[0] );
+
+    Logical closed_curve;
+
+    if ( dist( m_PntVec[0], m_PntVec.back() ) < ( m_BBox.DiagDist() * 1.0e-6 ) )
+    {
+        m_STEP_End_Vert = m_STEP_Start_Vert;
+        closed_curve = LTrue;
+    }
+    else
+    {
+        m_STEP_End_Vert = step->MakeVertex( m_PntVec.back() );
+        closed_curve = LFalse;
+    }
+
+    // Identify the edge
+    SdaiB_spline_curve_with_knots* curve = step->MakeCurve( m_PntVec, m_Deg, closed_curve, mergepnts, m_MergeTol );
+
+    SdaiEdge_curve* edge_crv = (SdaiEdge_curve*)step->registry->ObjCreate( "EDGE_CURVE" );
+    step->instance_list->Append( (SDAI_Application_instance*)edge_crv, completeSE );
+    edge_crv->edge_geometry_( curve );
+    edge_crv->edge_start_( m_STEP_Start_Vert );
+    edge_crv->edge_end_( m_STEP_End_Vert );
+    edge_crv->same_sense_( BTrue ); // direction set by oriented edge
+    edge_crv->name_( "''" );
+
+    m_STEP_Edge = edge_crv;
+}
--- src/cfd_mesh/NURBS.h
+++ src/cfd_mesh/NURBS.h
@@ -16,6 +16,70 @@
 #if !defined(NURBS_NURBS__INCLUDED_)
 #define NURBS_NURBS__INCLUDED_
 
+#include "SCurve.h"
+#include "STEPutil.h"
 
+
+// Describes a NURBS curve, formed from a Bezier surface curve (SCurve). Can be of border 
+// or intersection type.
+class NURBS_Curve
+{
+public:
+
+    NURBS_Curve();
+    virtual ~NURBS_Curve() {};
+
+    // Initialize the NURBS curve from an SCurve. Control points are extracted and 
+    // saved to m_control_pnts_xyz.
+    void InitNURBSCurve( SCurve curve );
+
+    // Define the NURBS curve as a SdaiEdge_curve
+    void WriteSTEPEdge( STEPutil* step, bool mergepnts );
+
+    // Defines m_IGES_Edge by as an IGES type 126 entity
+    void WriteIGESEdge( IGESutil* iges );
+
+    // Flag is true if the curve is a border curve
+    bool m_BorderFlag;
+
+    // Flag is true if the curve is an intersection curve
+    bool m_InternalFlag;
+
+    // Flag indicating the curve is a sub-surface curve
+    bool m_SubSurfFlag;
+
+    // Flag that identifies if the intersection curve is between two FEA Parts
+    bool m_StructIntersectFlag;
+
+    // Vector of points decribing the curve
+    vector < vec3d > m_PntVec;
+
+    // Parent surface indexes
+    int m_SurfA_ID;
+    int m_SurfB_ID;
+
+    // Pointers for the STEP representation of the NURBS curve
+    SdaiVertex_point* m_STEP_Start_Vert;
+    SdaiVertex_point* m_STEP_End_Vert;
+    SdaiEdge_curve* m_STEP_Edge;
+
+    // Pointer for the IGES representation of the NURBS curve
+    DLL_IGES_ENTITY_126* m_IGES_Edge;
+
+    // Type of intersection curve, such as transparent or structure
+    int m_SurfIntersectType;
+
+    // Relative tolerance for merging points through nanoflann based on parent surface bounding box
+    double m_MergeTol;
+
+    // Degree of the curve to define the NURBS curve
+    int m_Deg;
+
+protected:
+
+    // Bounding box of curve. Used to identify relative tolerances
+    BndBox m_BBox;
+
+};
 
 #endif
