4b31f56ee9d813a9b9d430997377b1f0742ef397
Justin Gravett <justin.gravett@esaero.com>
2020-06-03 10:58:30 -0700

    CFD Mesh API Test Suite Boiler Plate
    Creates bare CPP and header files with basic class definition and
    license header. The *.cpp file is included in the CMakeLists

---
+++ src/geom_api/APITestSuiteCFDMesh.cpp
@@ -0,0 +1,16 @@
+//
+// This file is released under the terms of the NASA Open Source Agreement (NOSA)
+// version 1.3 as detailed in the LICENSE file which accompanies this software.
+//
+// APITestSuiteCFDMesh.cpp: Unit tests for cfd_mesh
+//
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "VSP_Geom_API.h"
+#include "APITestSuiteCFDMesh.h"
+#include "APIDefines.h"
+
+//=============================================================================//
+//========================== APITestSuiteCFDMesh ==============================//
+//=============================================================================//
---
+++ src/geom_api/APITestSuiteCFDMesh.h
@@ -0,0 +1,25 @@
+//
+// This file is released under the terms of the NASA Open Source Agreement (NOSA)
+// version 1.3 as detailed in the LICENSE file which accompanies this software.
+//
+
+// APITestSuiteCFDMesh.h: Unit tests for geom_api
+//
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(VSPAPITESTSUITECFDMESH__INCLUDED_)
+#define VSPAPITESTSUITECFDMESH__INCLUDED_
+
+#include "cpptest.h"
+
+class APITestSuiteCFDMesh : public Test::Suite
+{
+public:
+
+
+private:
+
+};
+
+#endif // !defined(VSPAPITESTSUITECFDMESH__INCLUDED_)
