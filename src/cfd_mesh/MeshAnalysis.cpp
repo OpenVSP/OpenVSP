69cfe6dbc275ba6e5c56907b045e8485431b266d
garygrayESA <gary.gray@esaero.com>
2020-09-01 13:38:43 -0700

    Mesh Analysis Boiler Plate
    Adds empty files and includes in CMakeLists

---
+++ src/cfd_mesh/MeshAnalysis.cpp
@@ -0,0 +1,16 @@
+//
+// This file is released under the terms of the NASA Open Source Agreement (NOSA)
+// version 1.3 as detailed in the LICENSE file which accompanies this software.
+//
+
+// MeshAnalysis.cpp
+// Gary Gray, ESAero, 10/24/20
+//
+// Collection of analyses for the cfd_mesh project, similar to the AnalysisMgr in geom_core
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "MeshAnalysis.h"
+#include "StructureMgr.h"
+#include "FeaMeshMgr.h"
+
---
+++ src/cfd_mesh/MeshAnalysis.h
@@ -0,0 +1,18 @@
+//
+// This file is released under the terms of the NASA Open Source Agreement (NOSA)
+// version 1.3 as detailed in the LICENSE file which accompanies this software.
+//
+
+// MeshAnalysis.h
+// Gary Gray, ESAero, 10/24/20
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(MESHANALYSIS__INCLUDED_)
+#define MESHANALYSIS__INCLUDED_
+
+#include "AnalysisMgr.h"
+
+
+
+#endif // !defined(MESHANALYSIS__INCLUDED_)
