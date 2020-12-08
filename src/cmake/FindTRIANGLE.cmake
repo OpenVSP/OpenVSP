5a172bb7e99f756970f3e6683ff8e6149cdfaf51
justingravett <justin.gravett@esaero.com>
2020-12-07 19:55:53 -0800

    Adds CMake Support for Linking New Triangle to VSP Build Projects

---
+++ src/cmake/FindTRIANGLE.cmake
@@ -0,0 +1,60 @@
+# - Try to find the Triangle library
+#
+
+find_path( TRIANGLE_INCLUDE_DIR triangle.h 
+    HINTS
+        ${TRIANGLE_INSTALL_DIR}/include
+        ${TRIANGLE_INSTALL_DIR}
+        /usr/include
+        /usr/local/include
+        /sw/include
+        /opt/local/include
+        ${CMAKE_INSTALL_PREFIX}/include
+        DOC "The directory where triangle.h resides")
+
+find_path( TRIANGLE_API_INCLUDE_DIR triangle_api.h 
+    HINTS
+        ${TRIANGLE_INSTALL_DIR}/include
+        ${TRIANGLE_INSTALL_DIR}
+        /usr/include
+        /usr/local/include
+        /sw/include
+        /opt/local/include
+        ${CMAKE_INSTALL_PREFIX}/include
+        DOC "The directory where triangle_api.h resides")
+
+find_library( TRIANGLE_LIBRARY triangle
+    HINTS
+        ${TRIANGLE_INSTALL_DIR}/lib
+        ${TRIANGLE_INSTALL_DIR}
+        /usr/lib64
+        /usr/lib
+        /usr/local/lib64
+        /usr/local/lib
+        /sw/lib
+        /opt/local/lib
+        ${LIB_INSTALL_DIR}
+        DOC "The Triangle Library")
+
+find_library( TRIANGLE_API_LIBRARY triangle-api
+    HINTS
+        ${TRIANGLE_INSTALL_DIR}/lib
+        ${TRIANGLE_INSTALL_DIR}
+        /usr/lib64
+        /usr/lib
+        /usr/local/lib64
+        /usr/local/lib
+        /sw/lib
+        /opt/local/lib
+        ${LIB_INSTALL_DIR}
+        DOC "The Triangle API Library")
+
+set(TRIANGLE_INCLUDE_DIR ${TRIANGLE_INCLUDE_DIR} ${TRIANGLE_API_INCLUDE_DIR})
+set(TRIANGLE_LIBRARIES ${TRIANGLE_LIBRARY} ${TRIANGLE_API_LIBRARY})
+
+MESSAGE( STATUS "TRIANGLE_INCLUDE_DIR: " ${TRIANGLE_INCLUDE_DIR} )
+MESSAGE( STATUS "TRIANGLE_LIBRARIES: " ${TRIANGLE_LIBRARIES} )
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(TRIANGLE DEFAULT_MSG TRIANGLE_INCLUDE_DIR TRIANGLE_LIBRARIES )
+
