4d3c29a37e5282cc4503938c1a09d87fd277bc2b
Jason Welstead <jason.r.welstead@nasa.gov>
2021-10-06 13:48:55 -0400

    Added unit test for trim file functionality

--- src/python_api/packages/CHARM/charm/test/testcharminput.py
+++ src/python_api/packages/CHARM/charm/test/testcharminput.py
@@ -35,6 +35,61 @@ class Test(unittest.TestCase):
         print(blade_geom)
         self.assertTrue(True)
 
+    def test_build_default_trim_settings(self):
+        import charm.input_automation as charm
+        import numpy as np
+
+        trim_settings = charm.build_default_trim_settings(trim_template=os.path.join(path, "test_trim.inp"))
+
+        expFsMs = [1, 1, 1, 1, 1, 1]
+        self.assertEqual(expFsMs, trim_settings.forces_moments)
+
+        expTargets = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        self.assertEqual(expTargets, trim_settings.targets)
+
+        expTol = [.0002, .0002, .0002, .0002, .0002, .0002]
+        self.assertEqual(expTol, trim_settings.tol)
+
+        expPilotCntrls = [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0]
+        self.assertEqual(expPilotCntrls, trim_settings.pilot_cntrls)
+
+        expAttitudeCntls = [1, 1, 0]
+        self.assertEqual(expAttitudeCntls, trim_settings.attitude_cntrls)
+
+        expNumComp = 2
+        self.assertEqual(expNumComp, trim_settings.num_aero_comps)
+
+        expAeroCompCntrls = np.array([[1, 0, 1, 2, 3, 0, 0, 0, 0, 0],
+                                      [2, 0, 4, 0, 0, 0, 0, 0, 0, 0]])
+        self.assertSequenceEqual(expAeroCompCntrls.tolist(), trim_settings.aero_comp_cntrls.tolist())
+
+        expCntrlGains = np.array([[1, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                                  [2, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                                  [3, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                                  [4, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]])
+        self.assertSequenceEqual(expCntrlGains.tolist(), trim_settings.cntrl_gains.tolist())
+
+        expInitValues = [20.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        self.assertEqual(expInitValues, trim_settings.init_cntrls)
+
+# TODO: Need to create test that doesn't include trim inputs (uses test_rc.inp instead of test_rctrim.inp)
+    def test_read_run_characteristics_template(self):
+        import charm.input_automation as charm
+
+        template_filename = os.path.join(path, "test_rctrim.inp")
+        test_run_char_dict = charm.read_run_characteristics_template(template_filename=template_filename)
+
+        expNPSI = 24
+        self.assertEqual(expNPSI, test_run_char_dict['NPSI'])
+
+        expNROTOR = 2
+        self.assertEqual(expNROTOR, test_run_char_dict['NROTOR'])
+
+        expCG = [1.0, 2.0, 3.0]
+        self.assertEqual(expCG, [test_run_char_dict["XCG"],
+                                 test_run_char_dict["YCG"],
+                                 test_run_char_dict["ZCG"]])
+
 
 def create_geom():
     vsp.ClearVSPModel()
@@ -57,4 +112,6 @@ if __name__ == "__main__":
     # t.test_create_default_wing_settings()
     # t.test_create_default_rotor_settings()
     # t.test_create_charm_blade_geom_file_from_propeller()
+    # t.test_build_default_trim_settings()
+    # t.test_read_run_characteristics_template()
     unittest.main(verbosity=2)
