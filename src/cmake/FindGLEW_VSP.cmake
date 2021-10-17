07a5c6ecb1093f05ca231a15d4ea5d6212fcff9a
Rob McDonald <rob.a.mcdonald@gmail.com>
2021-10-16 20:47:05 -0700

    Clean up tabs from CMake files.

--- src/cmake/FindGLEW_VSP.cmake
+++ src/cmake/FindGLEW_VSP.cmake
@@ -8,34 +8,34 @@
 # 
 
 FIND_PATH( GLEW_INCLUDE_PATH
-	NAMES GL/glew.h
-	HINTS
-	${GLEW_INSTALL_DIR}
-	${GLEW_INSTALL_DIR}/include
-	/usr/include
-	/usr/local/include
-	/sw/include
-	/opt/local/include
-	${CMAKE_INSTALL_PREFIX}/include
-	DOC "The directory where GL/glew.h resides")
+    NAMES GL/glew.h
+    HINTS
+    ${GLEW_INSTALL_DIR}
+    ${GLEW_INSTALL_DIR}/include
+    /usr/include
+    /usr/local/include
+    /sw/include
+    /opt/local/include
+    ${CMAKE_INSTALL_PREFIX}/include
+    DOC "The directory where GL/glew.h resides")
 FIND_LIBRARY( GLEW_LIBRARY
-	NAMES glews glew glew32 glew32s GLEW
-	HINTS
-	${GLEW_INSTALL_DIR}
-	${GLEW_INSTALL_DIR}/lib
-	/usr/lib64
-	/usr/lib
-	/usr/local/lib64
-	/usr/local/lib
-	/sw/lib
-	/opt/local/lib
-	${LIB_INSTALL_DIR}
-	DOC "The GLEW library")
+    NAMES glews glew glew32 glew32s GLEW
+    HINTS
+    ${GLEW_INSTALL_DIR}
+    ${GLEW_INSTALL_DIR}/lib
+    /usr/lib64
+    /usr/lib
+    /usr/local/lib64
+    /usr/local/lib
+    /sw/lib
+    /opt/local/lib
+    ${LIB_INSTALL_DIR}
+    DOC "The GLEW library")
 
 IF (GLEW_INCLUDE_PATH)
-	SET( GLEW_FOUND 1 CACHE STRING "Set to 1 if GLEW is found, 0 otherwise")
+    SET( GLEW_FOUND 1 CACHE STRING "Set to 1 if GLEW is found, 0 otherwise")
 ELSE (GLEW_INCLUDE_PATH)
-	SET( GLEW_FOUND 0 CACHE STRING "Set to 1 if GLEW is found, 0 otherwise")
+    SET( GLEW_FOUND 0 CACHE STRING "Set to 1 if GLEW is found, 0 otherwise")
 ENDIF (GLEW_INCLUDE_PATH)
 
 MARK_AS_ADVANCED( GLEW_FOUND )
--- src/cmake/Findexprparse.cmake
+++ src/cmake/Findexprparse.cmake
@@ -8,34 +8,34 @@
 # 
 
 FIND_PATH( EXPRPARSE_INCLUDE_PATH
-	NAMES exprparse/exprparse.h
-	HINTS
-	${EXPRPARSE_INSTALL_DIR}
-	${EXPRPARSE_INSTALL_DIR}/include
-	/usr/include
-	/usr/local/include
-	/sw/include
-	/opt/local/include
-	${CMAKE_INSTALL_PREFIX}/include
-	DOC "The directory where exprparse/exprparse.h resides")
+    NAMES exprparse/exprparse.h
+    HINTS
+    ${EXPRPARSE_INSTALL_DIR}
+    ${EXPRPARSE_INSTALL_DIR}/include
+    /usr/include
+    /usr/local/include
+    /sw/include
+    /opt/local/include
+    ${CMAKE_INSTALL_PREFIX}/include
+    DOC "The directory where exprparse/exprparse.h resides")
 FIND_LIBRARY( EXPRPARSE_LIBRARY
-	NAMES exprparse
-	HINTS
-	${EXPRPARSE_INSTALL_DIR}
-	${EXPRPARSE_INSTALL_DIR}/lib
-	/usr/lib64
-	/usr/lib
-	/usr/local/lib64
-	/usr/local/lib
-	/sw/lib
-	/opt/local/lib
-	${LIB_INSTALL_DIR}
-	DOC "The exprparse library")
+    NAMES exprparse
+    HINTS
+    ${EXPRPARSE_INSTALL_DIR}
+    ${EXPRPARSE_INSTALL_DIR}/lib
+    /usr/lib64
+    /usr/lib
+    /usr/local/lib64
+    /usr/local/lib
+    /sw/lib
+    /opt/local/lib
+    ${LIB_INSTALL_DIR}
+    DOC "The exprparse library")
 
 IF (EXPRPARSE_INCLUDE_PATH)
-	SET( EXPRPARSE_FOUND 1 CACHE STRING "Set to 1 if exprparse is found, 0 otherwise")
+    SET( EXPRPARSE_FOUND 1 CACHE STRING "Set to 1 if exprparse is found, 0 otherwise")
 ELSE (EXPRPARSE_INCLUDE_PATH)
-	SET( EXPRPARSE_FOUND 0 CACHE STRING "Set to 1 if exprparse is found, 0 otherwise")
+    SET( EXPRPARSE_FOUND 0 CACHE STRING "Set to 1 if exprparse is found, 0 otherwise")
 ENDIF (EXPRPARSE_INCLUDE_PATH)
 
 MARK_AS_ADVANCED( EXPRPARSE_FOUND )
