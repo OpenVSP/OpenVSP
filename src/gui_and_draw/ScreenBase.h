a6e1583b66e84678b765b9d86eebc7c7100c2aa3
justingravett <justin.gravett@esaero.com>
2021-12-22 19:01:48 -0800

    Defines XSecScreen GUI Class for Geoms with XSec Tab
    This defines the class, derived from GeomScreen, but no screens have
    are updated to use this new class in this commit. This class will only
    be used to create the XSec tab and control the XSec type section of it.
    Special considerations are made for WingGeoms, which use "Airfoil"
    instead of "XSec". In addition, a flag is available to control when the
    new XSec driver parameterization should be made available.

--- src/gui_and_draw/ScreenBase.h
+++ src/gui_and_draw/ScreenBase.h
@@ -348,7 +348,260 @@ protected:
 };
 
 //==== Skin Screen ====//
-class SkinScreen : public GeomScreen
+class XSecScreen : public GeomScreen
+{
+public:
+    XSecScreen(ScreenMgr* mgr, int w, int h, const string & title);
+    virtual ~XSecScreen() {}
+
+    virtual bool Update();
+    virtual void CallBack(Fl_Widget *w);
+    virtual void GuiDeviceCallBack(GuiDevice* device);
+    static void staticScreenCB(Fl_Widget *w, void* data)
+    {
+        ((static_cast <XSecScreen*>(data))->CallBack(w));
+    }
+
+    void AddXSecLayout(bool include_point_type = true); // Default flag to include point type but allow PropGeom to skip it
+
+    virtual void DisplayGroup(GroupLayout* group);
+    virtual void RebuildCSTGroup(CSTAirfoil* cst_xs);
+
+protected:
+    GroupLayout* m_CurrDisplayGroup;
+
+    GroupLayout m_XSecLayout;
+
+    // Tab and divider pointers saved in memory so WingGeom can rename them
+    Fl_Group* m_XSecTab;
+    Fl_Box* m_XSecDivider;
+
+    IndexSelector m_XSecIndexSelector; // Not used for BORGeom
+
+    Choice m_XSecTypeChoice;
+    TriggerButton m_ShowXSecButton;
+
+    TriggerButton m_ConvertCEDITButton;
+    GroupLayout m_ConvertCEDITGroup;
+
+    HWXSecCurveDriverGroup m_DefaultXSecDriverGroup;
+    DXSecCurveDriverGroup m_CircleXSecDriverGroup;
+
+    GroupLayout m_EditCEDITGroup;
+    TriggerButton m_EditCEDITButton;
+
+    GroupLayout m_PointGroup;
+
+    GroupLayout m_SuperGroup;
+    DriverGroupBank m_SuperXSecDriverGroupBank;
+    SliderAdjRangeInput m_SuperHeightSlider;
+    SliderAdjRangeInput m_SuperWidthSlider;
+    SliderAdjRangeInput m_SuperMSlider;
+    SliderAdjRangeInput m_SuperNSlider;
+    ToggleButton m_SuperToggleSym;
+    SliderAdjRangeInput m_SuperM_botSlider;
+    SliderAdjRangeInput m_SuperN_botSlider;
+    SliderAdjRangeInput m_SuperMaxWidthLocSlider;
+
+    GroupLayout m_CircleGroup;
+    DriverGroupBank m_CircleXSecDriverGroupBank;
+    SliderAdjRangeInput m_DiameterSlider;
+
+    GroupLayout m_EllipseGroup;
+    DriverGroupBank m_EllipseXSecDriverGroupBank;
+    SliderAdjRangeInput m_EllipseHeightSlider;
+    SliderAdjRangeInput m_EllipseWidthSlider;
+
+    GroupLayout m_RoundedRectGroup;
+    DriverGroupBank m_RRXSecDriverGroupBank;
+    SliderAdjRangeInput m_RRHeightSlider;
+    SliderAdjRangeInput m_RRWidthSlider;
+    ToggleButton m_RRRadNoSymToggle;
+    ToggleButton m_RRRadRLSymToggle;
+    ToggleButton m_RRRadTBSymToggle;
+    ToggleButton m_RRRadAllSymToggle;
+    ToggleRadioGroup m_RRRadSymRadioGroup;
+    SliderAdjRangeInput m_RRRadiusBRSlider; // Bottom Right
+    SliderAdjRangeInput m_RRRadiusBLSlider; // Bottom Left
+    SliderAdjRangeInput m_RRRadiusTLSlider; // Top Left
+    SliderAdjRangeInput m_RRRadiusTRSlider; // Top Right
+    ToggleButton m_RRKeyCornerButton;
+    SliderAdjRangeInput m_RRSkewSlider;
+    SliderAdjRangeInput m_RRKeystoneSlider;
+    SliderAdjRangeInput m_RRVSkewSlider;
+
+    GroupLayout m_GenGroup;
+    DriverGroupBank m_GenXSecDriverGroupBank;
+    SliderAdjRangeInput m_GenHeightSlider;
+    SliderAdjRangeInput m_GenWidthSlider;
+    SliderAdjRangeInput m_GenMaxWidthLocSlider;
+    SliderAdjRangeInput m_GenCornerRadSlider;
+    SliderAdjRangeInput m_GenTopTanAngleSlider;
+    SliderAdjRangeInput m_GenBotTanAngleSlider;
+    SliderAdjRangeInput m_GenTopStrSlider;
+    SliderAdjRangeInput m_GenBotStrSlider;
+    SliderAdjRangeInput m_GenUpStrSlider;
+    SliderAdjRangeInput m_GenLowStrSlider;
+
+    GroupLayout m_FourSeriesGroup;
+    StringOutput m_FourNameOutput;
+    CheckButton m_FourInvertButton;
+    SliderAdjRangeInput m_FourChordSlider;
+    SliderAdjRangeInput m_FourThickChordSlider;
+    SliderAdjRangeInput m_FourCamberSlider;
+    SliderAdjRangeInput m_FourCLiSlider;
+    ToggleButton m_FourCamberButton;
+    ToggleButton m_FourCLiButton;
+    ToggleRadioGroup m_FourCamberGroup;
+    SliderAdjRangeInput m_FourCamberLocSlider;
+    CheckButton m_FourSharpTEButton;
+    TriggerButton m_FourFitCSTButton;
+    Counter m_FourDegreeCounter;
+
+    GroupLayout m_SixSeriesGroup;
+    StringOutput m_SixNameOutput;
+    CheckButton m_SixInvertButton;
+    Choice m_SixSeriesChoice;
+    SliderAdjRangeInput m_SixChordSlider;
+    SliderAdjRangeInput m_SixThickChordSlider;
+    SliderAdjRangeInput m_SixIdealClSlider;
+    SliderAdjRangeInput m_SixASlider;
+    TriggerButton m_SixFitCSTButton;
+    Counter m_SixDegreeCounter;
+
+    GroupLayout m_BiconvexGroup;
+    SliderAdjRangeInput m_BiconvexChordSlider;
+    SliderAdjRangeInput m_BiconvexThickChordSlider;
+
+    GroupLayout m_WedgeGroup;
+    SliderAdjRangeInput m_WedgeChordSlider;
+    SliderAdjRangeInput m_WedgeThickChordSlider;
+    SliderAdjRangeInput m_WedgeZCamberSlider;
+    ToggleButton m_WedgeSymmThickButton;
+    SliderAdjRangeInput m_WedgeThickLocSlider;
+    SliderAdjRangeInput m_WedgeThickLocLowSlider;
+    SliderAdjRangeInput m_WedgeFlatUpSlider;
+    SliderAdjRangeInput m_WedgeFlatLowSlider;
+    SliderAdjRangeInput m_WedgeUForeUpSlider;
+    SliderAdjRangeInput m_WedgeUForeLowSlider;
+    SliderAdjRangeInput m_WedgeDuUpSlider;
+    SliderAdjRangeInput m_WedgeDuLowSlider;
+    CheckButton m_WedgeInvertButton;
+
+    GroupLayout m_FuseFileGroup;
+    DriverGroupBank m_FuseFileXSecDriverGroupBank;
+    SliderAdjRangeInput m_FileHeightSlider;
+    SliderAdjRangeInput m_FileWidthSlider;
+    TriggerButton m_ReadFuseFileButton;
+
+    GroupLayout m_AfFileGroup;
+    TriggerButton m_AfReadFileButton;
+    StringOutput m_AfFileNameOutput;
+    CheckButton m_AfFileInvertButton;
+    SliderAdjRangeInput m_AfFileChordSlider;
+    SliderAdjRangeInput m_AfFileThickChordSlider;
+    Output m_AfFileBaseThickChordOutput;
+    TriggerButton m_AfFileFitCSTButton;
+    Counter m_AfFileDegreeCounter;
+
+    GroupLayout m_CSTAirfoilGroup;
+
+    Output m_CSTThickChordOutput;
+
+    SliderAdjRangeInput m_CSTChordSlider;
+
+    TriggerButton m_UpDemoteButton;
+    StringOutput m_UpDegreeOutput;
+    TriggerButton m_UpPromoteButton;
+
+    Fl_Scroll* m_CSTUpCoeffScroll;
+    GroupLayout m_CSTUpCoeffLayout;
+
+    TriggerButton m_LowDemoteButton;
+    StringOutput m_LowDegreeOutput;
+    TriggerButton m_LowPromoteButton;
+
+    vector < SliderAdjRangeInput > m_UpCoeffSliderVec;
+
+    Fl_Scroll* m_CSTLowCoeffScroll;
+    GroupLayout m_CSTLowCoeffLayout;
+
+    vector < SliderAdjRangeInput > m_LowCoeffSliderVec;
+
+    CheckButton m_CSTInvertButton;
+    CheckButton m_CSTContLERadButton;
+
+    GroupLayout m_VKTGroup;
+    SliderAdjRangeInput m_VKTChordSlider;
+    SliderAdjRangeInput m_VKTEpsilonSlider;
+    SliderAdjRangeInput m_VKTKappaSlider;
+    SliderAdjRangeInput m_VKTTauSlider;
+    CheckButton m_VKTInvertButton;
+    Output m_VKTThickChordOutput;
+    TriggerButton m_VKTFitCSTButton;
+    Counter m_VKTDegreeCounter;
+
+    GroupLayout m_FourDigitModGroup;
+    StringOutput m_FourModNameOutput;
+    CheckButton m_FourModInvertButton;
+    SliderAdjRangeInput m_FourModChordSlider;
+    SliderAdjRangeInput m_FourModThickChordSlider;
+    SliderAdjRangeInput m_FourModCamberSlider;
+    SliderAdjRangeInput m_FourModCLiSlider;
+    ToggleButton m_FourModCamberButton;
+    ToggleButton m_FourModCLiButton;
+    ToggleRadioGroup m_FourModCamberGroup;
+    SliderAdjRangeInput m_FourModCamberLocSlider;
+    SliderAdjRangeInput m_FourModThicknessLocSlider;
+    SliderAdjRangeInput m_FourModLERadIndexSlider;
+    CheckButton m_FourModSharpTEButton;
+    TriggerButton m_FourModFitCSTButton;
+    Counter m_FourModDegreeCounter;
+
+    GroupLayout m_FiveDigitGroup;
+    StringOutput m_FiveNameOutput;
+    CheckButton m_FiveInvertButton;
+    SliderAdjRangeInput m_FiveChordSlider;
+    SliderAdjRangeInput m_FiveThickChordSlider;
+    SliderAdjRangeInput m_FiveCLiSlider;
+    SliderAdjRangeInput m_FiveCamberLocSlider;
+    CheckButton m_FiveSharpTEButton;
+    TriggerButton m_FiveFitCSTButton;
+    Counter m_FiveDegreeCounter;
+
+    GroupLayout m_FiveDigitModGroup;
+    StringOutput m_FiveModNameOutput;
+    CheckButton m_FiveModInvertButton;
+    SliderAdjRangeInput m_FiveModChordSlider;
+    SliderAdjRangeInput m_FiveModThickChordSlider;
+    SliderAdjRangeInput m_FiveModCLiSlider;
+    SliderAdjRangeInput m_FiveModCamberLocSlider;
+    SliderAdjRangeInput m_FiveModThicknessLocSlider;
+    SliderAdjRangeInput m_FiveModLERadIndexSlider;
+    CheckButton m_FiveModSharpTEButton;
+    TriggerButton m_FiveModFitCSTButton;
+    Counter m_FiveModDegreeCounter;
+
+    GroupLayout m_OneSixSeriesGroup;
+    StringOutput m_OneSixSeriesNameOutput;
+    CheckButton m_OneSixSeriesInvertButton;
+    SliderAdjRangeInput m_OneSixSeriesChordSlider;
+    SliderAdjRangeInput m_OneSixSeriesThickChordSlider;
+    SliderAdjRangeInput m_OneSixSeriesCLiSlider;
+    SliderAdjRangeInput m_OneSixSeriesCamberLocSlider;
+    SliderAdjRangeInput m_OneSixSeriesThicknessLocSlider;
+    SliderAdjRangeInput m_OneSixSeriesLERadIndexSlider;
+    CheckButton m_OneSixSeriesSharpTEButton;
+    TriggerButton m_OneSixSeriesFitCSTButton;
+    Counter m_OneSixSeriesDegreeCounter;
+
+    // Variables used for slight differences in GUIs
+    bool m_XSecDriversActive; // Flag indicating if the width, area, etc. drivers should be shown
+
+};
+
+//==== Skin Screen ====//
+class SkinScreen : public XSecScreen
 {
 public:
     SkinScreen( ScreenMgr* mgr, int w, int h, const string & title );
@@ -397,7 +650,7 @@ protected:
 
 };
 
-class BlendScreen : public GeomScreen
+class BlendScreen : public XSecScreen
 {
 public:
     BlendScreen( ScreenMgr* mgr, int w, int h, const string & title );
