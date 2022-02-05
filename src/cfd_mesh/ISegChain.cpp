7485df70438f722b2ab714d146ccf37b02e4ad12
Rob McDonald <rob.a.mcdonald@gmail.com>
2022-02-05 13:24:25 -0800

    Document MergeSplits() routine a bit.

--- src/cfd_mesh/ISegChain.cpp
+++ src/cfd_mesh/ISegChain.cpp
@@ -921,6 +921,12 @@ bool ISegChain::AddBorderSplit( Puw* uw )
 //}
 
 
+// When splitting intersection chains (IntersectSplitChains) or border curves (SplitBorderCurves), compound
+// intersections can lead to identified split points that are separate, but should be coincident.  MergeSplits
+// identifies these splits and merges them.
+// Coincident splits are identified with a dimensional (model XYZ) tolerance.  This tolerance could likely be much
+// larger than it is as it is really a search radius.  We do not expect false positives to be very close at all.
+// When done, the first split identified of each group is used.  No averaging or smearing is currently performed.
 void ISegChain::MergeSplits()
 {
     int i, j;
