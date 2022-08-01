7e40cb99cdff83badbc84f25b7e15e63992d0916
Rob McDonald <rob.a.mcdonald@gmail.com>
2022-08-01 14:21:48 -0700

    Modify openvsp package init to set path to VSPAERO correctly

--- src/python_api/packages/openvsp/openvsp/__init__.py
+++ src/python_api/packages/openvsp/openvsp/__init__.py
@@ -3,3 +3,12 @@ from .degen_geom_parse import *
 from .parasite_drag import *
 from .surface_patches import *
 from .utilities import *
+import os
+
+def setup_vspaero_path():
+  base_dir = os.path.dirname(__file__)
+
+  if  vsp.CheckForVSPAERO( base_dir ):
+    vsp.SetVSPAEROPath( base_dir )
+
+setup_vspaero_path()
