8b8559c67f9af591338fa57226e85b8a946c3adb
Rob McDonald <rob.a.mcdonald@gmail.com>
2022-08-22 16:47:28 -0700

    Make test_NumAnalyses.py work with PyTest

--- src/test/py/tests/test_NumAnalyses.py
+++ src/test/py/tests/test_NumAnalyses.py
@@ -1,11 +1,10 @@
 import openvsp as vsp
 
+def test_NumAnalyses():
+    lbefore = vsp.ListAnalysis()
+    vsp.VSPRenew()
+    lafter = vsp.ListAnalysis()
+    assert len(lafter) == len(lbefore)
 
-lbefore = vsp.ListAnalysis()
-
-vsp.VSPRenew()
-
-lafter = vsp.ListAnalysis()
-
-assert len(lafter) == len(lbefore)
-
+if __name__ == "__main__":
+    test_NumAnalyses()
