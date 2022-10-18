f25804b30b05b825ff0bf71a8c54f7e7c3c81388
Rob McDonald <rob.a.mcdonald@gmail.com>
2022-10-18 10:02:52 -0700

    Shrink FEA Structure GUI back down to old width - also rename from FEA Mesh

--- src/gui_and_draw/StructScreen.cpp
+++ src/gui_and_draw/StructScreen.cpp
@@ -26,7 +26,7 @@ using namespace vsp;
 // Construction/Destruction
 //////////////////////////////////////////////////////////////////////
 
-StructScreen::StructScreen( ScreenMgr* mgr ) : TabScreen( mgr, 465, 720, "FEA Mesh", 196 )
+StructScreen::StructScreen( ScreenMgr* mgr ) : TabScreen( mgr, 430, 720, "FEA Structure", 196 )
 {
     m_FLTK_Window->callback( staticCloseCB, this );
 
