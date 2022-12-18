692d0b3258871e227bfc6a70258212b9cfbf9384
Rob McDonald <rob.a.mcdonald@gmail.com>
2022-12-17 23:40:25 -0800

    Use openvsp_config to control module loading instead of forcing vsp_g

--- src/python_api/packages/openvsp/openvsp/__init__.py
+++ src/python_api/packages/openvsp/openvsp/__init__.py
@@ -1,4 +1,13 @@
-from .vsp_g import *
+import openvsp_config
+
+if openvsp_config.LOAD_GRAPHICS:
+  if openvsp_config.LOAD_FACADE:
+    from .vsp_g_facade import *
+  else:
+    from .vsp_g import *
+else:
+  from .vsp import *
+
 from .degen_geom_parse import *
 from .parasite_drag import *
 from .surface_patches import *
