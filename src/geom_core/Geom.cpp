c8d746adf5a87e695024d36841b7d65192d57acc
Rob McDonald <rob.a.mcdonald@gmail.com>
2023-04-30 19:01:17 -0700

    Avoid UpdateAttachParms when parent geom type doesn't have surface.

--- src/geom_core/Geom.cpp
+++ src/geom_core/Geom.cpp
@@ -573,6 +573,16 @@ void GeomXForm::UpdateAttachParms()
 
     if ( parent )
     {
+        if ( parent->GetType().m_Type == MESH_GEOM_TYPE ||
+             parent->GetType().m_Type == WIRE_FRAME_GEOM_TYPE ||
+             parent->GetType().m_Type == BLANK_GEOM_TYPE ||
+             parent->GetType().m_Type == HINGE_GEOM_TYPE ||
+             parent->GetType().m_Type == HUMAN_GEOM_TYPE ||
+             parent->GetType().m_Type == PT_CLOUD_GEOM_TYPE )
+        {
+            return;
+        }
+
         double umax = parent->GetMainUMapMax( 0 );
         double lmax = parent->GetMainSurfPtr( 0 )->GetLMax();
 
