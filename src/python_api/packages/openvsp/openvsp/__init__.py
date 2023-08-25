bed0a9e7df8a3fa2f61b74c26505bdd01c27e101
M4Nascenzi <tnascenzi@m4-engineering.com>
2023-08-25 11:46:22 -0700

    fixed indentation in openvsp config

--- src/python_api/packages/openvsp/openvsp/__init__.py
+++ src/python_api/packages/openvsp/openvsp/__init__.py
@@ -1,31 +1,31 @@
 import openvsp_config
 
 if openvsp_config.LOAD_GRAPHICS:
-  if openvsp_config.LOAD_FACADE:
-    from .facade import *
-  elif openvsp_config.LOAD_MULTI_FACADE:
-	print("multi facade not supported")
-    #from .vsp_g_facade_multi import *
-  else:
-    from .vsp_g import *
+	if openvsp_config.LOAD_FACADE:
+		from .facade import *
+	elif openvsp_config.LOAD_MULTI_FACADE:
+		print("multi facade not supported")
+		#from .vsp_g_facade_multi import *
+	else:
+		from .vsp_g import *
 else:
-  from .vsp import *
+	from .vsp import *
 
 if openvsp_config._IGNORE_IMPORTS:
     pass
 
 elif not (openvsp_config.LOAD_FACADE or openvsp_config.LOAD_MULTI_FACADE):
 
-  from .degen_geom_parse import *
-  from .parasite_drag import *
-  from .surface_patches import *
-  from .utilities import *
-  import os
+	from .degen_geom_parse import *
+	from .parasite_drag import *
+	from .surface_patches import *
+	from .utilities import *
+	import os
 
-  def setup_vspaero_path():
-    base_dir = os.path.dirname(__file__)
+	def setup_vspaero_path():
+		base_dir = os.path.dirname(__file__)
 
-    if  CheckForVSPAERO( base_dir ):
-      SetVSPAEROPath( base_dir )
+		if  CheckForVSPAERO( base_dir ):
+			SetVSPAEROPath( base_dir )
 
-  setup_vspaero_path()
+	setup_vspaero_path()
