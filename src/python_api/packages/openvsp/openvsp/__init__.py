de96593080131cbd00207a111673d78047ef38af
Rob McDonald <rob.a.mcdonald@gmail.com>
2023-12-13 16:08:53 -0800

    Change tabs to spaces in openvsp/__init__.py  No functional changes.

--- src/python_api/packages/openvsp/openvsp/__init__.py
+++ src/python_api/packages/openvsp/openvsp/__init__.py
@@ -12,31 +12,31 @@ except ModuleNotFoundError:
     ignore_imports = False
 
 if load_graphics:
-	if load_facade:
-		from .facade import *
-	elif load_multi_facade:
-		print("multi facade not supported")
-		#from .vsp_g_facade_multi import *
-	else:
-		from .vsp_g import *
+    if load_facade:
+        from .facade import *
+    elif load_multi_facade:
+        print("multi facade not supported")
+        #from .vsp_g_facade_multi import *
+    else:
+        from .vsp_g import *
 else:
-	from .vsp import *
+    from .vsp import *
 
 if ignore_imports:
     pass
 
 elif not (load_facade or load_multi_facade):
 
-	from .degen_geom_parse import *
-	from .parasite_drag import *
-	from .surface_patches import *
-	from .utilities import *
-	import os
+    from .degen_geom_parse import *
+    from .parasite_drag import *
+    from .surface_patches import *
+    from .utilities import *
+    import os
 
-	def setup_vspaero_path():
-		base_dir = os.path.dirname(__file__)
+    def setup_vspaero_path():
+        base_dir = os.path.dirname(__file__)
 
-		if  CheckForVSPAERO( base_dir ):
-			SetVSPAEROPath( base_dir )
+        if  CheckForVSPAERO( base_dir ):
+            SetVSPAEROPath( base_dir )
 
-	setup_vspaero_path()
+    setup_vspaero_path()
