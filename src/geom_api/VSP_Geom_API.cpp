42b7adbfaaaed7899894bf7055f68efa5c88856d
Rob McDonald <rob.a.mcdonald@gmail.com>
2024-06-27 13:18:53 -0700

    Make API write FEA file after generating mesh.

--- src/geom_api/VSP_Geom_API.cpp
+++ src/geom_api/VSP_Geom_API.cpp
@@ -3336,6 +3336,9 @@ void ComputeFeaMesh( const string & geom_id, int fea_struct_ind, int file_type )
     FeaMeshMgr.SetFeaMeshStructID( feastruct->GetID() );
 
     FeaMeshMgr.GenerateFeaMesh();
+
+    FeaMeshMgr.ExportFeaMesh( feastruct->GetID() );
+
     ErrorMgr.NoError();
 }
 
@@ -3356,6 +3359,9 @@ void ComputeFeaMesh( const string & struct_id, int file_type )
     FeaMeshMgr.SetFeaMeshStructID( struct_id );
 
     FeaMeshMgr.GenerateFeaMesh();
+
+    FeaMeshMgr.ExportFeaMesh( struct_id );
+
     ErrorMgr.NoError();
 }
 
