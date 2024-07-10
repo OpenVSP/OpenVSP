e0774cfb70a44cda522dd286d35fdd2977c89032
Rob McDonald <rob.a.mcdonald@gmail.com>
2024-07-09 21:41:00 -0700

    Add example four-view screenshot script.

---
+++ src/test/py/tests/test_FourViewScreenShot.py
@@ -0,0 +1,48 @@
+import time
+import openvsp_config
+
+openvsp_config.LOAD_GRAPHICS = True
+openvsp_config.LOAD_FACADE = True
+import openvsp as vsp
+
+
+def test_FourViewScreenShot():
+    vsp.InitGUI()
+    vsp.StartGUI()
+
+    time.sleep(1)  # Needed or following API calls fail.
+
+    errorMgr = vsp.ErrorMgrSingleton.getInstance()
+
+    pod = vsp.AddGeom('POD', '')
+    vsp.SetGeomDrawType(pod, vsp.GEOM_DRAW_SHADE)
+    vsp.SetGeomMaterialName( pod, 'Ruby')
+
+    wing = vsp.AddGeom('WING', '')
+    vsp.SetGeomDrawType(wing, vsp.GEOM_DRAW_SHADE)
+    vsp.SetGeomMaterialName(wing, 'Jade')
+    xwing = vsp.GetParm(wing, 'X_Rel_Location', 'XForm')
+    vsp.SetParmVal(xwing, 2.25)
+
+    vsp.Update()
+
+    vsp.SetWindowLayout(2, 2)
+    vsp.FitAllViews()
+    vsp.UpdateGUI()
+
+    time.sleep(1)  # Needed or UpdateGUI() does not have time to complete,
+
+    png_path = 'favorite_airplane.png'
+    vsp.ScreenGrab(png_path, 400, 400, True)
+
+    # Check for errors
+    num_err = errorMgr.GetNumTotalErrors()
+    assert num_err == 0
+
+    for i in range(0, num_err):
+        err = errorMgr.PopLastError()
+        print('error = ', err.m_ErrorString)
+
+
+if __name__ == '__main__':
+    test_FourViewScreenShot()
