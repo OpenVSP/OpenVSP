4434db37312b783fce5acaad4426eb88363d666c
Rob McDonald <rob.a.mcdonald@gmail.com>
2024-09-09 12:50:13 -0700

    Initial ClearanceGeom and Screen - copy/paste from Conformal w/ name change

---
+++ src/geom_core/ClearanceGeom.h
@@ -0,0 +1,170 @@
+//
+// This file is released under the terms of the NASA Open Source Agreement (NOSA)
+// version 1.3 as detailed in the LICENSE file which accompanies this software.
+//
+
+// ClearanceGeom.h:
+// Rob McDonald
+//
+//////////////////////////////////////////////////////////////////////
+
+
+
+#if !defined(VSPCLEARANCEGEOM__INCLUDED_)
+#define VSPCLEARANCEGEOM__INCLUDED_
+
+#include "Defines.h"
+#include "Geom.h"
+#include "XSec.h"
+#include "XSecSurf.h"
+
+class ClearanceSpine;
+
+typedef piecewise_surface_type::piecewise_curve_type curve_type;
+
+
+//==== Clearance Geom ====//
+class ClearanceGeom : public Geom
+{
+public:
+    ClearanceGeom( Vehicle* vehicle_ptr );
+    virtual ~ClearanceGeom();
+
+    virtual void Scale();
+
+    Parm m_Offset;                  // Offset to Clearance Surface
+
+    BoolParm m_OffsetEnds;
+
+    IntParm m_UMinTrimTypeFlag;
+    IntParm m_UMaxTrimTypeFlag;
+
+    BoolParm m_UTrimFlag;
+    Parm m_UTrimMin;
+    Parm m_UTrimMax;
+
+    Parm m_LTrimMin;
+    BoolParm m_L01Min;
+    Parm m_L0LenTrimMin;
+
+    Parm m_LTrimMax;
+    BoolParm m_L01Max;
+    Parm m_L0LenTrimMax;
+
+    Parm m_EtaTrimMin;
+    Parm m_EtaTrimMax;
+
+    IntParm m_CapUMinTrimOption;
+    IntParm m_CapUMaxTrimOption;
+
+    Parm m_CapUMinTrimLength;
+    Parm m_CapUMinTrimOffset;
+    Parm m_CapUMinTrimStrength;
+    BoolParm m_CapUMinTrimSweepFlag;
+
+    Parm m_CapUMaxTrimLength;
+    Parm m_CapUMaxTrimOffset;
+    Parm m_CapUMaxTrimStrength;
+    BoolParm m_CapUMaxTrimSweepFlag;
+
+    BoolParm m_V1TrimFlag;
+    Parm m_V1TrimBegin;
+    Parm m_V1TrimEnd;
+
+    BoolParm m_V2TrimFlag;
+    Parm m_V2TrimBegin;
+    Parm m_V2TrimEnd;
+
+    BoolParm m_ChordTrimFlag;
+    Parm m_ChordTrimMin;
+    Parm m_ChordTrimMax;
+
+    BoolParm m_Side1TrimFlag;
+    Parm m_Side1Trim;
+
+    BoolParm m_Side2TrimFlag;
+    Parm m_Side2Trim;
+
+    BoolParm m_Side3TrimFlag;
+    Parm m_Side3Trim;
+
+    BoolParm m_Side4TrimFlag;
+    Parm m_Side4Trim;
+
+    bool IsWingParent()                             { return m_WingParentFlag; }
+
+
+protected:
+
+    virtual void UpdateSurf();
+    virtual void CopyDataFrom( Geom* geom_ptr );
+
+    virtual void UpdateDrawObj();
+    virtual void LoadDrawObjs( vector< DrawObj* > & draw_obj_vec );
+
+    virtual void OffsetCurve( curve_type & crv, double offset );
+    virtual void TranslateCurve( curve_type & crv, const vec3d & offset );
+    virtual vec3d ComputeCenter( curve_type & crv );
+    virtual void ReSkin( VspSurf & surf, vector< rib_data_type > & rib_vec );
+
+    virtual bool CheckIfRibIsPoint( rib_data_type & rib );
+
+    virtual void CenterRibCurves( VspSurf & surf, const VspSurf & ref_surf, double offset );
+    virtual void OffsetEndRibs( VspSurf & surf, double offset );
+    virtual void AdjustShape( VspSurf & surf, const VspSurf & ref_surf, double offset );
+
+    virtual double FindEndUOffsetCurve( VspSurf & surf, double offset, bool start_flag, curve_type & crv );
+    virtual double AdjustForSurfaceDist( const VspSurf & surf, const ClearanceSpine & spine, double u, double offset, bool backward_flag );
+
+    virtual void TrimU( VspSurf & surf );
+    virtual void TrimV( VspSurf & surf );
+
+    virtual void SetWingTrimParms( VspSurf & surf );
+    virtual void UpdateParms( VspSurf & surf );
+
+    virtual void FindDists( const VspSurf & surf, piecewise_curve_type & curve, double u0, vector< double > & dist_vec );
+    virtual double ComputeAvgOffset( VspSurf & surf, const VspSurf & ref_surf, double u );
+
+    virtual double ComputeMaxOffsetError( VspSurf & surf, VspSurf &  ref_surf, double offset, int num_u, int num_w );
+
+
+    bool m_WingParentFlag;
+
+};
+
+
+
+class ClearanceSpine
+{
+public:
+
+    ClearanceSpine()
+    {
+        m_MaxDist = 0;
+    }
+    virtual ~ClearanceSpine()            {};
+
+    void Build( const VspSurf & surf );
+
+    int GetNumSamps() const               { return m_DistVec.size(); }
+    double GetMaxDist() const             { return m_MaxDist; }
+    double FindUGivenDist( double d ) const;
+    vec3d FindCenterGivenU( double u ) const;
+
+
+    double FindUGivenLengthAlongSpine( double length ) const;
+    double GetSpineLength() const;
+
+protected:
+
+    vector< double > m_UVec;
+    vector< vec3d > m_CenterVec;
+    vector< double > m_DistVec;
+
+    double m_MaxDist;
+};
+
+
+
+
+#endif // !defined(VSPCLEARANCEGEOM__INCLUDED_)
---
+++ src/gui_and_draw/ClearanceScreen.h
@@ -0,0 +1,113 @@
+//
+// This file is released under the terms of the NASA Open Source Agreement (NOSA)
+// version 1.3 as detailed in the LICENSE file which accompanies this software.
+//
+
+// ClearanceScreen.h: UI for Clearance Geom
+// Rob McDonald
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(CLEARANCESCREEN__INCLUDED_)
+#define CLEARANCESCREEN__INCLUDED_
+
+#include "ScreenBase.h"
+#include "GuiDevice.h"
+
+#include <FL/Fl.H>
+
+class ClearanceScreen : public GeomScreen
+{
+public:
+    ClearanceScreen( ScreenMgr* mgr );
+    virtual ~ClearanceScreen()                            {}
+
+    virtual void Show();
+    virtual bool Update();
+
+    virtual void CallBack( Fl_Widget *w );
+
+protected:
+
+    GroupLayout m_DesignLayout;
+    GroupLayout m_TrimGroup;
+    GroupLayout m_SideGroup;
+    GroupLayout m_WingGroup;
+
+    SliderAdjRangeInput m_OffsetSlider;
+
+    ToggleButton m_OffsetEndsToggle;
+
+    ToggleButton m_UTrimToggle;
+    SliderAdjRangeInput m_UTrimMinSlider;
+    SliderAdjRangeInput m_UTrimMaxSlider;
+
+    ToggleButton m_UMinUTrimButton;
+    ToggleButton m_UminLTrimButton;
+    ToggleButton m_UMinEtaTrimButton;
+    ToggleRadioGroup m_UMinTrimToggleGroup;
+
+    ToggleButton m_L01MinTrimToggle;
+    ToggleButton m_L0LenMinTrimToggle;
+    ToggleRadioGroup m_LMinScaleTrimToggleGroup;
+
+    SliderAdjRange2Input m_LTrimMinSlider;
+
+    ToggleButton m_UMaxUTrimButton;
+    ToggleButton m_UmaxLTrimButton;
+    ToggleButton m_UmaxEtaTrimButton;
+    ToggleRadioGroup m_UMaxTrimToggleGroup;
+
+    ToggleButton m_L01MaxTrimToggle;
+    ToggleButton m_L0LenMaxTrimToggle;
+    ToggleRadioGroup m_LMaxScaleTrimToggleGroup;
+
+    SliderAdjRange2Input m_LTrimMaxSlider;
+
+    SliderAdjRangeInput m_EtaTrimMinSlider;
+    SliderAdjRangeInput m_EtaTrimMaxSlider;
+
+    Choice m_NoseCapTypeChoice;
+    SliderAdjRangeInput m_NoseCapLenSlider;
+    SliderAdjRangeInput m_NoseCapOffsetSlider;
+    SliderAdjRangeInput m_NoseCapStrengthSlider;
+    ToggleButton m_NoseCapSweepFlagButton;
+
+    Choice m_TailCapTypeChoice;
+    SliderAdjRangeInput m_TailCapLenSlider;
+    SliderAdjRangeInput m_TailCapOffsetSlider;
+    SliderAdjRangeInput m_TailCapStrengthSlider;
+    ToggleButton m_TailCapSweepFlagButton;
+
+    ToggleButton m_V1TrimToggle;
+    SliderAdjRangeInput m_V1TrimMinSlider;
+    SliderAdjRangeInput m_V1TrimMaxSlider;
+
+    ToggleButton m_V2TrimToggle;
+    SliderAdjRangeInput m_V2TrimMinSlider;
+    SliderAdjRangeInput m_V2TrimMaxSlider;
+
+    //ToggleButton m_SpanTrimToggle;
+    //SliderAdjRangeInput m_SpanTrimMinSlider;
+    //SliderAdjRangeInput m_SpanTrimMaxSlider;
+
+    ToggleButton m_ChordTrimToggle;
+    SliderAdjRangeInput m_ChordTrimMinSlider;
+    SliderAdjRangeInput m_ChordTrimMaxSlider;
+
+    ToggleButton m_Side1TrimToggle;
+    SliderAdjRangeInput m_Side1TrimSlider;
+
+    ToggleButton m_Side2TrimToggle;
+    SliderAdjRangeInput m_Side2TrimSlider;
+
+    ToggleButton m_Side3TrimToggle;
+    SliderAdjRangeInput m_Side3TrimSlider;
+
+    ToggleButton m_Side4TrimToggle;
+    SliderAdjRangeInput m_Side4TrimSlider;
+
+};
+
+
+#endif // !defined(CLEARANCESCREEN__INCLUDED_)
