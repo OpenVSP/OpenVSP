798a155a586d0936183d7c933c41fc289a81c867
Rob McDonald <rob.a.mcdonald@gmail.com>
2024-09-09 13:45:33 -0700

    Strip ClearanceGeom and Screen down to empty shell

--- src/geom_core/ClearanceGeom.h
+++ src/geom_core/ClearanceGeom.h
@@ -18,10 +18,6 @@
 #include "XSec.h"
 #include "XSecSurf.h"
 
-class ClearanceSpine;
-
-typedef piecewise_surface_type::piecewise_curve_type curve_type;
-
 
 //==== Clearance Geom ====//
 class ClearanceGeom : public Geom
@@ -30,68 +26,13 @@ public:
     ClearanceGeom( Vehicle* vehicle_ptr );
     virtual ~ClearanceGeom();
 
+    virtual void ComputeCenter();
+
     virtual void Scale();
 
-    Parm m_Offset;                  // Offset to Clearance Surface
+    virtual void AddDefaultSources( double base_len = 1.0 );
 
-    BoolParm m_OffsetEnds;
-
-    IntParm m_UMinTrimTypeFlag;
-    IntParm m_UMaxTrimTypeFlag;
-
-    BoolParm m_UTrimFlag;
-    Parm m_UTrimMin;
-    Parm m_UTrimMax;
-
-    Parm m_LTrimMin;
-    BoolParm m_L01Min;
-    Parm m_L0LenTrimMin;
-
-    Parm m_LTrimMax;
-    BoolParm m_L01Max;
-    Parm m_L0LenTrimMax;
-
-    Parm m_EtaTrimMin;
-    Parm m_EtaTrimMax;
-
-    IntParm m_CapUMinTrimOption;
-    IntParm m_CapUMaxTrimOption;
-
-    Parm m_CapUMinTrimLength;
-    Parm m_CapUMinTrimOffset;
-    Parm m_CapUMinTrimStrength;
-    BoolParm m_CapUMinTrimSweepFlag;
-
-    Parm m_CapUMaxTrimLength;
-    Parm m_CapUMaxTrimOffset;
-    Parm m_CapUMaxTrimStrength;
-    BoolParm m_CapUMaxTrimSweepFlag;
-
-    BoolParm m_V1TrimFlag;
-    Parm m_V1TrimBegin;
-    Parm m_V1TrimEnd;
-
-    BoolParm m_V2TrimFlag;
-    Parm m_V2TrimBegin;
-    Parm m_V2TrimEnd;
-
-    BoolParm m_ChordTrimFlag;
-    Parm m_ChordTrimMin;
-    Parm m_ChordTrimMax;
-
-    BoolParm m_Side1TrimFlag;
-    Parm m_Side1Trim;
-
-    BoolParm m_Side2TrimFlag;
-    Parm m_Side2Trim;
-
-    BoolParm m_Side3TrimFlag;
-    Parm m_Side3Trim;
-
-    BoolParm m_Side4TrimFlag;
-    Parm m_Side4Trim;
-
-    bool IsWingParent()                             { return m_WingParentFlag; }
+    virtual void OffsetXSecs( double off );
 
 
 protected:
@@ -102,69 +43,6 @@ protected:
     virtual void UpdateDrawObj();
     virtual void LoadDrawObjs( vector< DrawObj* > & draw_obj_vec );
 
-    virtual void OffsetCurve( curve_type & crv, double offset );
-    virtual void TranslateCurve( curve_type & crv, const vec3d & offset );
-    virtual vec3d ComputeCenter( curve_type & crv );
-    virtual void ReSkin( VspSurf & surf, vector< rib_data_type > & rib_vec );
-
-    virtual bool CheckIfRibIsPoint( rib_data_type & rib );
-
-    virtual void CenterRibCurves( VspSurf & surf, const VspSurf & ref_surf, double offset );
-    virtual void OffsetEndRibs( VspSurf & surf, double offset );
-    virtual void AdjustShape( VspSurf & surf, const VspSurf & ref_surf, double offset );
-
-    virtual double FindEndUOffsetCurve( VspSurf & surf, double offset, bool start_flag, curve_type & crv );
-    virtual double AdjustForSurfaceDist( const VspSurf & surf, const ClearanceSpine & spine, double u, double offset, bool backward_flag );
-
-    virtual void TrimU( VspSurf & surf );
-    virtual void TrimV( VspSurf & surf );
-
-    virtual void SetWingTrimParms( VspSurf & surf );
-    virtual void UpdateParms( VspSurf & surf );
-
-    virtual void FindDists( const VspSurf & surf, piecewise_curve_type & curve, double u0, vector< double > & dist_vec );
-    virtual double ComputeAvgOffset( VspSurf & surf, const VspSurf & ref_surf, double u );
-
-    virtual double ComputeMaxOffsetError( VspSurf & surf, VspSurf &  ref_surf, double offset, int num_u, int num_w );
-
-
-    bool m_WingParentFlag;
-
 };
-
-
-
-class ClearanceSpine
-{
-public:
-
-    ClearanceSpine()
-    {
-        m_MaxDist = 0;
-    }
-    virtual ~ClearanceSpine()            {};
-
-    void Build( const VspSurf & surf );
-
-    int GetNumSamps() const               { return m_DistVec.size(); }
-    double GetMaxDist() const             { return m_MaxDist; }
-    double FindUGivenDist( double d ) const;
-    vec3d FindCenterGivenU( double u ) const;
-
-
-    double FindUGivenLengthAlongSpine( double length ) const;
-    double GetSpineLength() const;
-
-protected:
-
-    vector< double > m_UVec;
-    vector< vec3d > m_CenterVec;
-    vector< double > m_DistVec;
-
-    double m_MaxDist;
-};
-
-
-
 
 #endif // !defined(VSPCLEARANCEGEOM__INCLUDED_)
