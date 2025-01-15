eeff252174d2ed97e62b93b4f5ba73bccf04c21d
tcuatt <tcuatt@m4-engineering.com>
2025-01-15 11:17:36 -0800

    Add VspTextEditor and label controls to GuiDevice     ParmButton label can be relabeled and font varied     Font changes to purple when parm contains an attribute     VspTextEditor is derived class of Fl_TextEditor         adds hotkey support for callback on "enter"         better for attribute entry

--- src/gui_and_draw/GuiDevice.cpp
+++ src/gui_and_draw/GuiDevice.cpp
@@ -1642,6 +1642,29 @@ void TriggerButton::SetColor( Fl_Color c )
     }
 }
 
+void TriggerButton::SetLabel( const char* label )
+{
+    if ( m_Button )
+    {
+        m_Button->copy_label( label );
+    }
+}
+
+void TriggerButton::SetShortcut( int s, bool append_label )
+{
+    if ( m_Button )
+    {
+        m_Button->shortcut( s );
+        if ( append_label )
+        {
+            string label = m_Button->label();
+            string chars = fl_shortcut_label( s );
+            label += " (" + chars + ")";
+            SetLabel( label.c_str() );
+        }
+    }
+}
+
 void TriggerButton::SetLabelColor( Fl_Color c )
 {
     if ( m_Button )
@@ -2249,6 +2272,31 @@ void StringOutput::Update( const string & val )
 {
     m_String = val;
     m_Output->value( m_String.c_str() );
+}
+
+//=====================================================================//
+//=========== Text Editor Widget With Enter Key Callback    ===========//
+//=====================================================================//
+
+VspTextEditor::VspTextEditor(int X, int Y, int W, int H,  const char* l) : Fl_Text_Editor( X, Y, W, H, l)
+{
+
+}
+
+static void kill_selection(Fl_Text_Editor* e) {
+  if (e->buffer()->selected()) {
+    e->insert_position(e->buffer()->primary_selection()->start());
+    e->buffer()->remove_selection();
+  }
+}
+
+int VspTextEditor::kf_accept(int, Fl_Text_Editor* e)
+{
+    kill_selection(e);
+    e->show_insert_position();
+    e->set_changed();
+    e->do_callback(FL_REASON_CHANGED);
+    return 1;
 }
 
 //=====================================================================//
--- src/gui_and_draw/GuiDevice.h
+++ src/gui_and_draw/GuiDevice.h
@@ -28,6 +28,7 @@
 #include <FL/Fl_Tree_Item.H>
 #include <FL/Fl_Tree_Prefs.H>
 #include <FL/Fl_Scroll.H>
+#include <FL/Fl_Text_Editor.H>
 
 #include "Cartesian.H"
 
@@ -494,6 +495,8 @@ public:
     virtual void Init( VspScreen* screen, Fl_Button* button );
     virtual void DeviceCB( Fl_Widget* w );
     virtual void SetColor( Fl_Color c );
+    void SetLabel( const char* label );
+    void SetShortcut( int s, bool append_label = false );
     virtual void SetLabelColor( Fl_Color c );
     virtual Fl_Button* GetFlButton();
 
@@ -867,6 +870,15 @@ protected:
     Fl_Output* m_Output;
 };
 
+class VspTextEditor : public Fl_Text_Editor
+{
+public:
+    VspTextEditor( int X, int Y, int W, int H,  const char* l );
+
+    typedef int (*Key_Func)(int key, Fl_Text_Editor* editor);
+
+    static int kf_accept(int, Fl_Text_Editor*);
+};
 
 //==== Index Selector  ====//
 class IndexSelector : public GuiDevice
