ece073836bd6cab12f4ab285e48056f49a2725da
Rob McDonald <rob.a.mcdonald@gmail.com>
2025-05-02 09:15:41 -0700

    Add TMesh::MaxDistanceRay()

--- src/geom_core/TMesh.cpp
+++ src/geom_core/TMesh.cpp
@@ -750,6 +750,11 @@ double TMesh::MinDistance( const vec3d &org, const vec3d &norm, double curr_min_
     return m_TBox.MinDistance( org, norm, curr_min_dist, p1, p2 );
 }
 
+double TMesh::MaxDistanceRay( const vec3d &org, const vec3d &norm, double curr_min_dist, vec3d &p1, vec3d &p2 )
+{
+    return m_TBox.MaxDistanceRay( org, norm, curr_min_dist, p1, p2 );
+}
+
 double TMesh::MinAngle( const vec3d &org, const vec3d &norm, const vec3d& ptaxis, const vec3d& axis, double curr_min_angle, int ccw, vec3d &p1, vec3d &p2 )
 {
     return m_TBox.MinAngle( org, norm, ptaxis, axis, curr_min_angle, ccw, p1, p2 );
