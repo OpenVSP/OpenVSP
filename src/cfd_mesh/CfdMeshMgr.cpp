dcf3f4e178b604176814bb4d0fc768ccb64c30f2
Rob McDonald <rob.a.mcdonald@gmail.com>
2026-01-13 17:25:52 -0800

    Don't write number of points twice to vspgeom from CFDMesh
    This should have been removed when the prior line was added, but it was not.

--- src/cfd_mesh/CfdMeshMgr.cpp
+++ src/cfd_mesh/CfdMeshMgr.cpp
@@ -1791,9 +1791,6 @@ void CfdMeshMgrSingleton::WriteNASCART_Obj_Tri_Gmsh( const string &dat_fn, const
             fprintf( fp, "1\n" );  // Number of meshes.
             fprintf( fp, "%d %d %d\n", allUsedPntVec.size(), nface, wakes.size() );
 
-            //==== Write Pnt Count ====//
-            fprintf( fp, "%d\n", ( int )allUsedPntVec.size() );
-
             //==== Write Pnts ====//
             for ( int i = 0 ; i < ( int )allUsedPntVec.size() ; i++ )
             {
