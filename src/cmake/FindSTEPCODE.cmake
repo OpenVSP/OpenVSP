3f306878c08ae3b45dee5bdc44addaeb76a33837
Rob McDonald <rob.a.mcdonald@gmail.com>
2026-02-18 15:55:46 -0800

    Update FindSTEPCODE.cmake

--- src/cmake/FindSTEPCODE.cmake
+++ src/cmake/FindSTEPCODE.cmake
@@ -4,12 +4,11 @@
 
 IF(NOT WIN32)
     set( STEPCODE_LIBRARIES
-    ${STEPCODE_INSTALL_DIR}/lib/libsdai_ap203.a
-    ${STEPCODE_INSTALL_DIR}/lib/libstepeditor.a
-    ${STEPCODE_INSTALL_DIR}/lib/libstepcore.a
-    ${STEPCODE_INSTALL_DIR}/lib/libstepdai.a
-    ${STEPCODE_INSTALL_DIR}/lib/libsteputils.a
-    ${STEPCODE_INSTALL_DIR}/lib/libbase.a )
+    ${STEPCODE_INSTALL_DIR}/lib/libsdai_ap203-static.a
+    ${STEPCODE_INSTALL_DIR}/lib/libstepeditor-static.a
+    ${STEPCODE_INSTALL_DIR}/lib/libstepcore-static.a
+    ${STEPCODE_INSTALL_DIR}/lib/libstepdai-static.a
+    ${STEPCODE_INSTALL_DIR}/lib/libsteputils-static.a )
 ELSE()
     set( STEPCODE_LIBRARIES
     ${STEPCODE_INSTALL_DIR}/lib/libexpress.lib
@@ -23,6 +22,7 @@ ELSE()
     shlwapi.lib )
 ENDIF()
 
+#${STEPCODE_INSTALL_DIR}/lib/libbase-static.a
 
 set( STEPCODE_INCLUDE_DIR
 ${STEPCODE_INSTALL_DIR}/include/stepcode
