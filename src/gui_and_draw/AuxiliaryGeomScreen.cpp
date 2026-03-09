840c9c63c6ca1b28f821742defa9ca5191d89911
Rob McDonald <rob.a.mcdonald@gmail.com>
2026-03-09 14:54:28 -0700

    Remove tire contact type from SingleBogie aux geom

--- src/gui_and_draw/AuxiliaryGeomScreen.cpp
+++ src/gui_and_draw/AuxiliaryGeomScreen.cpp
@@ -191,12 +191,6 @@ AuxiliaryGeomScreen::AuxiliaryGeomScreen( ScreenMgr* mgr ) : GeomScreen( mgr, 40
     m_SingleBogie1SuspensionModeChoice.AddItem( "Extended", vsp::GEAR_SUSPENSION_EXTENDED );
     m_SingleBogie1SuspensionModeChoice.UpdateItems();
 
-    m_SingleBogie1TireModeChoice.AddItem( "Static Load", vsp::TIRE_STATIC_LODED_CONTACT );
-    m_SingleBogie1TireModeChoice.AddItem( "Unloaded", vsp::TIRE_NOMINAL_CONTACT );
-    m_SingleBogie1TireModeChoice.AddItem( "Grown", vsp::TIRE_GROWTH_CONTACT );
-    m_SingleBogie1TireModeChoice.AddItem( "Flat", vsp::TIRE_FLAT_CONTACT );
-    m_SingleBogie1TireModeChoice.UpdateItems();
-
     m_SingleBogie1ClearanceModeChoice.AddItem( "Nominal", vsp::TIRE_NOMINAL_CLEARANCE );
     m_SingleBogie1ClearanceModeChoice.AddItem( "Grown", vsp::TIRE_GROWTH_CLEARANCE );
     m_SingleBogie1ClearanceModeChoice.AddItem( "Clearance", vsp::TIRE_CLEARANCE );
@@ -207,7 +201,6 @@ AuxiliaryGeomScreen::AuxiliaryGeomScreen( ScreenMgr* mgr ) : GeomScreen( mgr, 40
     m_SingleGearLayout.AddSlider( m_SingleKRetractSlider, "Retract", 10, "%5.4f" );
     m_SingleGearLayout.AddChoice( m_SingleBogie1SymmChoice, "I Symm" );
     m_SingleGearLayout.AddChoice( m_SingleBogie1SuspensionModeChoice, "Suspension Mode" );
-    m_SingleGearLayout.AddChoice( m_SingleBogie1TireModeChoice, "Tire Mode" );
     m_SingleGearLayout.AddChoice( m_SingleBogie1ClearanceModeChoice, "Clearance Mode" );
 
 
@@ -1010,7 +1003,6 @@ bool AuxiliaryGeomScreen::Update()
 
             m_SingleBogie1SymmChoice.Update( auxiliary_ptr->m_ContactPt1_Isymm.GetID() );
             m_SingleBogie1SuspensionModeChoice.Update( auxiliary_ptr->m_ContactPt1_SuspensionMode.GetID() );
-            m_SingleBogie1TireModeChoice.Update( auxiliary_ptr->m_ContactPt1_TireMode.GetID() );
             m_SingleBogie1ClearanceModeChoice.Update( auxiliary_ptr->m_ContactPt1_ClearanceMode.GetID() );
             m_SingleBogie1GearModeChoice.Update( auxiliary_ptr->m_ContactPt1_GearMode.GetID() );
             m_SingleKRetractSlider.Update( auxiliary_ptr->m_ContactPt1_KRetract.GetID() );
