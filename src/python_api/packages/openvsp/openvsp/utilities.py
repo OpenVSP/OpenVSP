d31d5c9bdc3e3ed1c8e5b07523e29da48c0b8a29
Rob McDonald <rob.a.mcdonald@gmail.com>
2026-05-01 11:30:00 -0700

    Update plot_propeller_info to newer matplotlib API

--- src/python_api/packages/openvsp/openvsp/utilities.py
+++ src/python_api/packages/openvsp/openvsp/utilities.py
@@ -221,14 +221,14 @@ def plot_propeller_info(prop_infos, vector_scale=10.0, markersize=2.0, mutation_
 
     class Arrow3D(FancyArrowPatch):
         def __init__(self, xs, ys, zs, *args, **kwargs):
+            super().__init__((0, 0), (0, 0), *args, **kwargs)
             self._verts3d = xs, ys, zs
-            FancyArrowPatch.__init__(self, (0, 0), (0, 0), *args, **kwargs)
 
-        def draw(self, renderer):
+        def do_3d_projection(self, renderer=None):
             xs3d, ys3d, zs3d = self._verts3d
-            xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
+            xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, self.axes.M)
             self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
-            FancyArrowPatch.draw(self, renderer)
+            return np.min(zs)
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
