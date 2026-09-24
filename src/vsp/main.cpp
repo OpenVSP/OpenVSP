49c90e0edaac1bddef0b7daac7481a4fdcfbd82d
Rob McDonald <rob.a.mcdonald@gmail.com>
2026-09-24 08:56:19 -0700

    Remove dependence on nanohttp and online version ping

--- src/vsp/main.cpp
+++ src/vsp/main.cpp
@@ -15,7 +15,7 @@
 #endif
 
 #include <libxml/tree.h>
-#include <libxml/nanohttp.h>
+// #include <libxml/nanohttp.h> // Removed in libxml2 2.14, see CheckVersionNumber
 
 #include <functional>
 
@@ -37,162 +37,167 @@
 using namespace vsp;
 using namespace std;
 
-bool ExtractVersionNumber( string & str, int* major, int* minor, int* change )
-{
-    *major = *minor = *change = -1;
-    //==== Find Leading String ====//
-    string target( "The latest release of OpenVSP is version " );
-    size_t start = str.find( target ) + target.size();
-    size_t end = str.find( ".\n", start );
-
-    //==== Just The Number =====//
-    string verstr = str.substr ( start, end - start );
-
-    if ( verstr.size() < 5 || verstr.size() > 9 )
-    {
-        return false;
-    }
-
-    size_t period1 = verstr.find( '.' );
-    size_t period2 = verstr.find( '.', period1 + 1 );
-
-    //==== Extract Major/Minor/Change Numbers ====//
-    *major  = atoi( verstr.substr( 0, period1 ).c_str() );
-    *minor  = atoi( verstr.substr( period1 + 1, period2 - ( period1 + 1 ) ).c_str() );
-    *change = atoi( verstr.substr( period2 + 1, verstr.size() ).c_str() );
-
-    return *major >= 0 && *minor >= 0 && *change >= 0;
-}
-
-// Simple version message function to be executed by main thread
-// when commanded by version check thread.
-//void newver_msg( void *data ) {
-//  if ( screenMgrPtr )
-//      screenMgrPtr->MessageBox("A new version of OpenVSP is available at http://www.openvsp.org/");
-//}
-
-#ifdef WIN32
-DWORD WINAPI CheckVersionNumber( LPVOID lpParameter )
-#else
-void* CheckVersionNumber( void *threadid )
-#endif
-
-{
-    //==== Init Nano HTTP ====//
-    xmlNanoHTTPInit();
-    xmlNanoHTTPScanProxy( nullptr );
-
-    //==== Compute Version Number ====//
-    int ver_no = 10000 * VSP_VERSION_MAJOR + 100 * VSP_VERSION_MINOR + VSP_VERSION_CHANGE;
-
-    char cCurrentPath[FILENAME_MAX];
-    GetCurrentDir( cCurrentPath, sizeof( cCurrentPath ) );
-
-    unsigned long user_id = ( unsigned long )std::hash<std::string>{}( string( cCurrentPath ) );
-
-    //==== Post User Info To Server ====//
-    char poststr[256];
-    snprintf( poststr, sizeof( poststr ), "postvar1=%lu&postvar2=%d\r\n", user_id, ver_no );
-    int poststrlen = strlen( poststr );
-    const char*  headers = "Content-Type: application/x-www-form-urlencoded \n";
-
-    void * ctx = 0;
-    ctx = xmlNanoHTTPMethod( "http://www.openvsp.org/vspuse_post.php", "POST", poststr, nullptr, headers, poststrlen );
-
-    if ( ctx )
-    {
-        xmlNanoHTTPClose( ctx );
-    }
-
-    ctx = 0;
-
-    //==== Open Settings File ====//
-    bool check_version_flag = true;
-    FILE* vsptime_fp = fopen( ".vsptime", "r" );
-    if ( vsptime_fp )
-    {
-        char str[256];
-        fgets( str, 256, vsptime_fp );
-        int vsp_time = atoi( str );
-        int del_time =  ( int )time( nullptr ) - vsp_time;
-        if ( del_time < 60 * 60 * 24 * 7 )          // Check Every Week
-        {
-            check_version_flag = false;
-        }
-
-        fclose( vsptime_fp );
-    }
-
-    //==== Enough Time Has Passed - Check For New Version ====//
-    if ( check_version_flag )
-    {
-        //==== Webpage with Version Info ====//
-        char * pContentType = 0;
-        ctx = xmlNanoHTTPOpen( "http://www.openvsp.org/latest_version3.html", &pContentType );
-
-        int retCode = xmlNanoHTTPReturnCode( ctx );
-
-        //==== Http Return Code 200 -> OK  ====//
-        string contentStr;
-        if ( retCode == 200 )
-        {
-            char buf[2048];
-            int len = 1;
-            while ( len > 0 && contentStr.size() < 10000 )
-            {
-                len = xmlNanoHTTPRead( ctx, buf, sizeof( buf ) );
-                contentStr.append( buf, len );
-            }
-        }
-
-        //==== Pulled A String From Server =====//
-        if ( contentStr.size() > 0 )
-        {
-            int major_ver, minor_ver, change_ver;
-            bool valid = ExtractVersionNumber( contentStr, &major_ver, &minor_ver, &change_ver );
-
-            if ( valid )
-            {
-                if ( major_ver != VSP_VERSION_MAJOR || minor_ver != VSP_VERSION_MINOR || change_ver != VSP_VERSION_CHANGE )
-                {
-                    // Send message to main thread to display new version message.
-                    printf( "A new version is available\n" );
-                    GuiInterface::getInstance().PopupMsg( "A new version of OpenVSP is available at http://www.openvsp.org/" );
-                }
-            }
-        }
-        //===== Write Time =====//
-        FILE* vsptime_fpw = fopen( ".vsptime", "w" );
-        if ( vsptime_fpw )
-        {
-            fprintf( vsptime_fpw, "%d", ( int )time( nullptr ) );
-            fclose( vsptime_fpw );
-        }
-    }
-
-    if ( ctx )
-    {
-        xmlNanoHTTPClose( ctx );
-    }
-
-    xmlNanoHTTPCleanup();
-
-    return 0;
-}
-
-void ThreadCheckVersionNumber()
-{
-#ifdef WIN32
-
-    DWORD myThreadID;
-    HANDLE myHandle = CreateThread( 0, 0, CheckVersionNumber, 0, 0, &myThreadID );
-#else
-
-    long t = 0;
-    pthread_t thread;
-    pthread_create( &thread, nullptr, CheckVersionNumber, ( void * )t );
-#endif
-}
+// The online version check and usage report below are disabled.  They used
+// libxml2's nanoHTTP client, which libxml2 2.14 removed, and plain HTTP,
+// while openvsp.org now redirects to HTTPS and no longer serves the version
+// page.  Kept for reference until they are reimplemented.
+//
+// bool ExtractVersionNumber( string & str, int* major, int* minor, int* change )
+// {
+//     *major = *minor = *change = -1;
+//     //==== Find Leading String ====//
+//     string target( "The latest release of OpenVSP is version " );
+//     size_t start = str.find( target ) + target.size();
+//     size_t end = str.find( ".\n", start );
+//
+//     //==== Just The Number =====//
+//     string verstr = str.substr ( start, end - start );
+//
+//     if ( verstr.size() < 5 || verstr.size() > 9 )
+//     {
+//         return false;
+//     }
+//
+//     size_t period1 = verstr.find( '.' );
+//     size_t period2 = verstr.find( '.', period1 + 1 );
+//
+//     //==== Extract Major/Minor/Change Numbers ====//
+//     *major  = atoi( verstr.substr( 0, period1 ).c_str() );
+//     *minor  = atoi( verstr.substr( period1 + 1, period2 - ( period1 + 1 ) ).c_str() );
+//     *change = atoi( verstr.substr( period2 + 1, verstr.size() ).c_str() );
+//
+//     return *major >= 0 && *minor >= 0 && *change >= 0;
+// }
+//
+// // Simple version message function to be executed by main thread
+// // when commanded by version check thread.
+// //void newver_msg( void *data ) {
+// //  if ( screenMgrPtr )
+// //      screenMgrPtr->MessageBox("A new version of OpenVSP is available at http://www.openvsp.org/");
+// //}
+//
+// #ifdef WIN32
+// DWORD WINAPI CheckVersionNumber( LPVOID lpParameter )
+// #else
+// void* CheckVersionNumber( void *threadid )
+// #endif
+//
+// {
+//     //==== Init Nano HTTP ====//
+//     xmlNanoHTTPInit();
+//     xmlNanoHTTPScanProxy( nullptr );
+//
+//     //==== Compute Version Number ====//
+//     int ver_no = 10000 * VSP_VERSION_MAJOR + 100 * VSP_VERSION_MINOR + VSP_VERSION_CHANGE;
+//
+//     char cCurrentPath[FILENAME_MAX];
+//     GetCurrentDir( cCurrentPath, sizeof( cCurrentPath ) );
+//
+//     unsigned long user_id = ( unsigned long )std::hash<std::string>{}( string( cCurrentPath ) );
+//
+//     //==== Post User Info To Server ====//
+//     char poststr[256];
+//     snprintf( poststr, sizeof( poststr ), "postvar1=%lu&postvar2=%d\r\n", user_id, ver_no );
+//     int poststrlen = strlen( poststr );
+//     const char*  headers = "Content-Type: application/x-www-form-urlencoded \n";
+//
+//     void * ctx = 0;
+//     ctx = xmlNanoHTTPMethod( "http://www.openvsp.org/vspuse_post.php", "POST", poststr, nullptr, headers, poststrlen );
+//
+//     if ( ctx )
+//     {
+//         xmlNanoHTTPClose( ctx );
+//     }
+//
+//     ctx = 0;
+//
+//     //==== Open Settings File ====//
+//     bool check_version_flag = true;
+//     FILE* vsptime_fp = fopen( ".vsptime", "r" );
+//     if ( vsptime_fp )
+//     {
+//         char str[256];
+//         fgets( str, 256, vsptime_fp );
+//         int vsp_time = atoi( str );
+//         int del_time =  ( int )time( nullptr ) - vsp_time;
+//         if ( del_time < 60 * 60 * 24 * 7 )          // Check Every Week
+//         {
+//             check_version_flag = false;
+//         }
+//
+//         fclose( vsptime_fp );
+//     }
+//
+//     //==== Enough Time Has Passed - Check For New Version ====//
+//     if ( check_version_flag )
+//     {
+//         //==== Webpage with Version Info ====//
+//         char * pContentType = 0;
+//         ctx = xmlNanoHTTPOpen( "http://www.openvsp.org/latest_version3.html", &pContentType );
+//
+//         int retCode = xmlNanoHTTPReturnCode( ctx );
+//
+//         //==== Http Return Code 200 -> OK  ====//
+//         string contentStr;
+//         if ( retCode == 200 )
+//         {
+//             char buf[2048];
+//             int len = 1;
+//             while ( len > 0 && contentStr.size() < 10000 )
+//             {
+//                 len = xmlNanoHTTPRead( ctx, buf, sizeof( buf ) );
+//                 contentStr.append( buf, len );
+//             }
+//         }
+//
+//         //==== Pulled A String From Server =====//
+//         if ( contentStr.size() > 0 )
+//         {
+//             int major_ver, minor_ver, change_ver;
+//             bool valid = ExtractVersionNumber( contentStr, &major_ver, &minor_ver, &change_ver );
+//
+//             if ( valid )
+//             {
+//                 if ( major_ver != VSP_VERSION_MAJOR || minor_ver != VSP_VERSION_MINOR || change_ver != VSP_VERSION_CHANGE )
+//                 {
+//                     // Send message to main thread to display new version message.
+//                     printf( "A new version is available\n" );
+//                     GuiInterface::getInstance().PopupMsg( "A new version of OpenVSP is available at http://www.openvsp.org/" );
+//                 }
+//             }
+//         }
+//         //===== Write Time =====//
+//         FILE* vsptime_fpw = fopen( ".vsptime", "w" );
+//         if ( vsptime_fpw )
+//         {
+//             fprintf( vsptime_fpw, "%d", ( int )time( nullptr ) );
+//             fclose( vsptime_fpw );
+//         }
+//     }
+//
+//     if ( ctx )
+//     {
+//         xmlNanoHTTPClose( ctx );
+//     }
+//
+//     xmlNanoHTTPCleanup();
+//
+//     return 0;
+// }
+//
+// void ThreadCheckVersionNumber()
+// {
+// #ifdef WIN32
+//
+//     DWORD myThreadID;
+//     HANDLE myHandle = CreateThread( 0, 0, CheckVersionNumber, 0, 0, &myThreadID );
+// #else
+//
+//     long t = 0;
+//     pthread_t thread;
+//     pthread_create( &thread, nullptr, CheckVersionNumber, ( void * )t );
+// #endif
+// }
 
 //========================================================//
 //========================================================//
@@ -219,7 +224,8 @@ int main( int argc, char** argv )
     GuiInterface::getInstance().InitGUI( vPtr );
 
     //==== Check Server For Version Number ====//
-    ThreadCheckVersionNumber();
+    // Disabled, see CheckVersionNumber above.
+    // ThreadCheckVersionNumber();
 
     //==== Run Test Scripts =====//
 #ifndef NDEBUG
